f10444d9f17fa1993f80a8a3c60b6cbd4cf4fa9e
Simon Baese <simon@youvo.org>
2021-08-02 12:40:22 +0200

    Add deploy script file

---
+++ scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Exit on error.
+set -e
+
+# Sync working branch with origin.
+git fetch
+git reset --hard origin/$(git branch | grep \* | cut -d ' ' -f2)
+git clean -df
+
+# Install Composer dependecies.
+composer install --no-dev --no-interaction --no-progress --no-suggest
