2c22d10eb657b0e3055a66a699c1006350480eb0
Simon Baese <simon@youvo.org>
2021-08-09 16:16:15 +0200

    Add delete script for test environment

--- scripts/delete-drupal.sh
+++ scripts/delete-drupal.sh
@@ -1,7 +1,23 @@
  #!/bin/bash 
- cd ..
- docker-compose down
- cd web
- bash reset-sites.sh
- cd ..
- docker-compose up -d 
+ 
+ # Delete database
+ mysql -e "DROP DATABASE IF EXISTS youvo_test"
+ mysql -e "CREATE DATABASE youvo_test"
+ echo $'\033[0;32m\033[1mDatabase reset!\033[0m\n'
+ 
+ # Set permissions
+ cd ../web/sites
+ chmod 0644 default
+ 
+ # Remove settings file
+ cd default
+ chmod 0777 settings.php
+ rm settings.php
+ echo $'\033[0;32m\033[1mSettings deleted!\033[0m\n'
+ 
+ # Reset files folder
+ rm -rf files
+ mkdir files
+ chmod 0777 files
+ echo $'\033[0;32m\033[1mFiles folder reinitialized!\033[0m\n'
+ 
