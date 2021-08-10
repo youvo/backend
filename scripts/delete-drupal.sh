a23e73e977ea5fa375971109e40c4a72e0eae269
Simon Baese <simon@youvo.org>
2021-08-10 11:55:55 +0200

    Introduce delete script

--- scripts/delete-drupal.sh
+++ scripts/delete-drupal.sh
@@ -1,23 +1,48 @@
  #!/bin/bash 
  
+ SITE_FOLDER=_test-backend
+ PATH=$PATH:~/www/$SITE_FOLDER/vendor/bin
+
+ cd ~
+ cd www/$SITE_FOLDER
+ set -a; source conf/.env; set +a
+ 
  # Delete database
- mysql -e "DROP DATABASE IF EXISTS youvo_test"
- mysql -e "CREATE DATABASE youvo_test"
- echo $'\033[0;32m\033[1mDatabase reset!\033[0m\n'
+ mysql -e "DROP DATABASE IF EXISTS ${DB_NAME}"
+ mysql -e "CREATE DATABASE ${DB_NAME}"
+ echo $'\033[1mDatabase reset!\033[0m'
  
  # Set permissions
- cd ../web/sites
+ cd web/sites
  chmod 0755 default
  
  # Remove settings file
  cd default
  chmod 0777 settings.php
  rm settings.php
- echo $'\033[0;32m\033[1mSettings deleted!\033[0m\n'
+ cp default.settings.php settings.php
+ chmod 0755 settings.php
+ echo $'\033[1mSettings reset!\033[0m'
  
  # Reset files folder
  rm -rf files
  mkdir files
  chmod 0777 files
- echo $'\033[0;32m\033[1mFiles folder reinitialized!\033[0m\n'
+ echo $'\033[1mFiles folder reset!\033[0m'
+  
+ # Reinstall drupal
+ cd ~
+ cd www/$SITE_FOLDER/web
+ drush site:install -y youvo_platform \
+  --locale="en" \
+  --db-url="${DB_DRIVER}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}" \
+  --site-name="youvo.org" \
+  --site-mail="hello@youvo.org" \
+  --account-name="admin" \
+  --account-mail="hello@youvo.org" \
+  --account-pass="admin"
+ 
+ drush cr
+ echo $'\033[1mExit in 3 seconds!\033[0m'
+ sleep 3
  
