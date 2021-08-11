8baa396933ce36c4cc42d59ddfeac6897baf0676
Simon Baese <simon@youvo.org>
2021-08-11 17:43:32 +0200

    Add cache rebuild at the end of deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
+ #!/bin/bash
 
-# Exit on error.
-set -e
+ # Exit on error.
+ set -e
 
-# Sync working branch with origin.
-git fetch
-git reset --hard origin/$(git branch | grep \* | cut -d ' ' -f2)
-git clean -df
+ # Sync working branch with origin.
+ git fetch
+ git reset --hard origin/$(git branch | grep \* | cut -d ' ' -f2)
+ git clean -df
 
-# Install Composer dependecies.
-composer install --no-interaction --no-progress
+ # Install Composer dependecies.
+ composer install --no-interaction --no-progress
+
+ # Rebuild Cache.
+ drush cr > /dev/null 2>&1
