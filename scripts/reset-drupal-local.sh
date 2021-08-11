f851e9d96a5b99fb238b156adefe218c5f61f244
Simon Baese <simon@youvo.org>
2021-08-11 17:43:51 +0200

    Add error handlers to reset scripts

--- scripts/reset-drupal-local.sh
+++ scripts/reset-drupal-local.sh
@@ -1,26 +1,29 @@
  #!/bin/bash 
  
- # Check if sudo user
+ # Exit on error.
+ set -e
+ 
+ # Check if sudo user.
  if [ "$UID" != "$ROOT_UID" ] 
  then 
    	echo "Please run using sudo!"
 	exit
  fi
  
- # Navigate to directory
+ # Navigate to directory.
  SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
  cd $SCRIPTPATH
  
- # Stop docker containers
+ # Stop docker containers.
  cd ..
  echo "Stopping docker containers ..."
  docker-compose down > /dev/null 2>&1
  
- # Set permissions
+ # Set permissions.
  cd web/sites
  chmod 0755 default
  
- # Remove settings file
+ # Remove settings file.
  cd default
  chmod 0777 settings.php
  rm settings.php
@@ -28,22 +31,22 @@
  chmod 0777 settings.php
  echo "Settings reset ..."
  
- # Reset files folder
+ # Reset files folder.
  chmod 0777 files
  rm -rf files
  mkdir files
  chmod 0777 files
  echo "Files folder reset ..."
  
- # Start docker containers
+ # Start docker containers.
  cd ../../..
  echo "Starting docker containers ..."
  docker-compose up -d > /dev/null 2>&1
  
- # Wait for containers to be accessible
+ # Wait for containers to be accessible.
  sleep 5
  
- # Reinstall drupal
+ # Reinstall drupal.
  echo "Installing Drupal ..."
  make drush "si -y youvo_development \
   --locale=en \
@@ -54,11 +57,11 @@
   --account-mail=hello@youvo.org \
   --account-pass=admin" > /dev/null 2>&1
  
- # Rebuild Cache
+ # Rebuild Cache.
  echo "Rebuilding Cache ..."
  make drush cr > /dev/null 2>&1
  
- # Bye bye
+ # Bye bye.
  echo "Success!"
  sleep 1
  
