26408702c5823f13182e3c09788b7183f9dbfe18
Simon Baese <simon@youvo.org>
2021-08-25 18:29:53 +0200

    Introduce Bundle Class for projects as youvo_projects module

---
+++ web/modules/youvo/youvo_projects/src/Entity/Project.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Drupal\youvo_projects\Entity;
+
+use Drupal\node\Entity\Node;
+use Drupal\youvo_projects\ProjectInterface;
+
+/**
+ *
+ */
+class Project extends Node implements ProjectInterface {
+
+  /**
+   * Implement whatever business logic specific to basic pages.
+   */
+  public function getState() {
+    return $this->get('field_lifecycle')->value;
+  }
+
+}
---
+++ web/modules/youvo/youvo_projects/src/ProjectInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\youvo_projects;
+
+use Drupal\node\NodeInterface;
+
+/**
+ * Provides an interface defining a project node entity.
+ */
+interface ProjectInterface extends NodeInterface {
+
+}
