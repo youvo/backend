59f1fcae44bc61de66dda3d5c6d917d7c234a892
Simon Baese <simon@youvo.org>
2021-08-26 17:48:14 +0200

    Amend access control for ProjectMediateForm

--- web/modules/youvo/youvo_lifecycle/src/Controller/AccessTransitionController.php
+++ web/modules/youvo/youvo_lifecycle/src/Controller/AccessTransitionController.php
@@ -3,24 +3,34 @@
 namespace Drupal\youvo_lifecycle\Controller;
 
 use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Controller\ControllerBase;
 use Drupal\Core\Session\AccountInterface;
 
 /**
  * Access controller for transition forms.
  */
-class AccessTransitionController {
+class AccessTransitionController extends ControllerBase {
 
   /**
    * Checks access for a specific request.
    *
    * @param \Drupal\Core\Session\AccountInterface $account
    *   Run access checks for this account.
+   * @param int|null $nid
+   *   The node id.
    *
    * @return \Drupal\Core\Access\AccessResultInterface
    *   The access results.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
    */
-  public function accessProjectMediate(AccountInterface $account) {
-    return AccessResult::allowedIf($account->hasPermission('use project_lifecycle transition project_mediate'));
+  public function accessProjectMediate(AccountInterface $account, int $nid = NULL) {
+
+    /** @var \Drupal\youvo_projects\Entity\Project $project */
+    $project = $this->entityTypeManager()->getStorage('node')->load($nid);
+
+    return AccessResult::allowedIf($account->hasPermission('use project_lifecycle transition project_mediate') && $project->canMediate());
   }
 
 }
