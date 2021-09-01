b093193cd145fa12f4a0603ec5bd8848e78e53db
Simon Baese <simon@youvo.org>
2021-09-01 15:37:16 +0200

    Refactor project param converter

--- web/modules/youvo/youvo_projects/src/ParamConverter/ProjectParamConverter.php
+++ web/modules/youvo/youvo_projects/src/ParamConverter/ProjectParamConverter.php
@@ -20,14 +20,12 @@ class ProjectParamConverter extends EntityConverter implements ParamConverterInt
    */
   public function convert($value, $definition, $name, array $defaults) {
 
-    if ($name === 'project') {
-      if ($this->isRestRequest($defaults) && Uuid::isValid($value)) {
-        $entity_type_id = $this->getEntityTypeFromDefaults($definition, $name, $defaults);
-        $query = \Drupal::entityQuery($entity_type_id)
-          ->condition('uuid', $value)
-          ->execute();
-        $value = reset($query);
-      }
+    if ($this->isRestRequest($defaults) && Uuid::isValid($value)) {
+      $entity_type_id = $this->getEntityTypeFromDefaults($definition, $name, $defaults);
+      $query = \Drupal::entityQuery($entity_type_id)
+        ->condition('uuid', $value)
+        ->execute();
+      $value = reset($query);
     }
 
     return parent::convert($value, $definition, $name, $defaults);
@@ -40,9 +38,7 @@ class ProjectParamConverter extends EntityConverter implements ParamConverterInt
    *   The route defaults array.
    */
   private function isRestRequest(array $defaults) {
-    /** @var \Symfony\Component\Routing\Route $route_object */
-    $route_object = $defaults['_route_object'];
-    $valid_methods = $route_object->getMethods();
+    $valid_methods = $defaults['_route_object']->getMethods();
     $method = substr($defaults['_route'], strrpos($defaults['_route'], ".") + 1);
     return in_array($method, $valid_methods);
   }
