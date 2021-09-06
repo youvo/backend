4e4e0187d979a94cbc9d4807b4129b80390790b9
Simon Baese <simon@youvo.org>
2021-09-06 12:55:58 +0200

    Introduce base module for academy

---
+++ web/modules/academy/academy/src/EventSubscriber/AcademySubscriber.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\academy\EventSubscriber;
+
+use Drupal\Core\Messenger\MessengerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Academy event subscriber.
+ */
+class AcademySubscriber implements EventSubscriberInterface {
+
+  /**
+   * The messenger.
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
+  protected $messenger;
+
+  /**
+   * Constructs event subscriber.
+   *
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   *   The messenger.
+   */
+  public function __construct(MessengerInterface $messenger) {
+    $this->messenger = $messenger;
+  }
+
+  /**
+   * Kernel request event handler.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\RequestEvent $event
+   *   Response event.
+   */
+  public function onKernelRequest(RequestEvent $event) {
+    $this->messenger->addStatus(__FUNCTION__);
+  }
+
+  /**
+   * Kernel response event handler.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\ResponseEvent $event
+   *   Response event.
+   */
+  public function onKernelResponse(ResponseEvent $event) {
+    $this->messenger->addStatus(__FUNCTION__);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    return [
+      KernelEvents::REQUEST => ['onKernelRequest'],
+      KernelEvents::RESPONSE => ['onKernelResponse'],
+    ];
+  }
+
+}
