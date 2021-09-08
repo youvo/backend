afeaa3d139b5fbf5dde9d9b67c1f753351ef6773
Simon Baese <simon@youvo.org>
2021-09-08 12:13:56 +0200

    Rework access handler for paragraph entity as child entity

--- web/modules/academy/child_entities/src/ChildEntityAccessControlHandler.php
+++ web/modules/academy/child_entities/src/ChildEntityAccessControlHandler.php
@@ -2,9 +2,9 @@
 
 namespace Drupal\child_entities;
 
+use Drupal\Core\Access\AccessException;
 use Drupal\Core\Entity\EntityAccessControlHandler;
 use Drupal\Core\Entity\EntityInterface;
-use Drupal\Core\Entity\EntityPublishedInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\Core\Access\AccessResult;
 
@@ -22,30 +22,26 @@ class ChildEntityAccessControlHandler extends EntityAccessControlHandler {
     // Allowed when the operation is not view or the status is true.
     /** @var \Drupal\child_entities\ChildEntityInterface $entity */
 
-    if ($operation === 'view' && is_subclass_of($entity, EntityPublishedInterface::class)) {
-      $access_result = AccessResult::allowedIf($entity->isPublished());
+    if ($operation == 'delete' && $entity->isNew()) {
+      return AccessResult::forbidden()->addCacheableDependency($entity);
+    }
+
+    if ($admin_permission = $this->entityType->getAdminPermission()) {
+      $access_result = AccessResult::allowedIfHasPermission($account, $admin_permission);
     }
     else {
-      $access_result = AccessResult::allowed();
+      $access_result = AccessResult::neutral();
     }
 
     if ($entity->getParentEntity() != NULL) {
       $parent_access = $entity->getParentEntity()->access($operation, $account, TRUE);
       $access_result = $access_result->andIf($parent_access);
     }
+    else {
+      throw new AccessException('Could not resolve parent access handler.');
+    }
 
     return $access_result;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function checkCreateAccess(AccountInterface $account, array $context, $entity_bundle = NULL) {
-    // Allow paragraph entities to be created in the context of entity forms.
-    if (\Drupal::requestStack()->getCurrentRequest()->getRequestFormat() === 'html') {
-      return AccessResult::allowed()->addCacheContexts(['request_format']);
-    }
-    return AccessResult::neutral()->addCacheContexts(['request_format']);
   }
 
 }
