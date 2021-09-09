0d59f0f3858764c9fded0262fa5f754213775dc0
Simon Baese <simon@youvo.org>
2021-09-09 11:05:10 +0200

    Add boilerplate for quizzes module

---
+++ web/modules/academy/quizzes/src/Entity/Question.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace Drupal\quizzes\Entity;
+
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityChangedTrait;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\quizzes\QuestionInterface;
+use Drupal\user\UserInterface;
+
+/**
+ * Defines the question entity class.
+ *
+ * @ContentEntityType(
+ *   id = "question",
+ *   label = @Translation("Question"),
+ *   label_collection = @Translation("Questions"),
+ *   bundle_label = @Translation("Question type"),
+ *   handlers = {
+ *     "view_builder" = "Drupal\quizzes\QuestionViewBuilder",
+ *     "list_builder" = "Drupal\quizzes\QuestionListBuilder",
+ *     "views_data" = "Drupal\views\EntityViewsData",
+ *     "form" = {
+ *       "add" = "Drupal\quizzes\Form\QuestionForm",
+ *       "edit" = "Drupal\quizzes\Form\QuestionForm",
+ *       "delete" = "Drupal\Core\Entity\ContentEntityDeleteForm"
+ *     },
+ *     "route_provider" = {
+ *       "html" = "Drupal\Core\Entity\Routing\AdminHtmlRouteProvider",
+ *     }
+ *   },
+ *   base_table = "question",
+ *   data_table = "question_field_data",
+ *   translatable = TRUE,
+ *   admin_permission = "administer question types",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "langcode" = "langcode",
+ *     "bundle" = "bundle",
+ *     "label" = "id",
+ *     "uuid" = "uuid"
+ *   },
+ *   links = {
+ *     "add-form" = "/admin/content/question/add/{question_type}",
+ *     "add-page" = "/admin/content/question/add",
+ *     "canonical" = "/question/{question}",
+ *     "edit-form" = "/admin/content/question/{question}/edit",
+ *     "delete-form" = "/admin/content/question/{question}/delete",
+ *     "collection" = "/admin/content/question"
+ *   },
+ *   bundle_entity_type = "question_type",
+ *   field_ui_base_route = "entity.question_type.edit_form"
+ * )
+ */
+class Question extends ContentEntityBase implements QuestionInterface {
+
+  use EntityChangedTrait;
+
+  /**
+   * {@inheritdoc}
+   *
+   * When a new question entity is created, set the uid entity reference to
+   * the current user as the creator of the entity.
+   */
+  public static function preCreate(EntityStorageInterface $storage_controller, array &$values) {
+    parent::preCreate($storage_controller, $values);
+    $values += ['uid' => \Drupal::currentUser()->id()];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCreatedTime() {
+    return $this->get('created')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setCreatedTime($timestamp) {
+    $this->set('created', $timestamp);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getOwner() {
+    return $this->get('uid')->entity;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getOwnerId() {
+    return $this->get('uid')->target_id;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setOwnerId($uid) {
+    $this->set('uid', $uid);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setOwner(UserInterface $account) {
+    $this->set('uid', $account->id());
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+
+    $fields = parent::baseFieldDefinitions($entity_type);
+
+    $fields['description'] = BaseFieldDefinition::create('text_long')
+      ->setTranslatable(TRUE)
+      ->setLabel(t('Description'))
+      ->setDescription(t('A description of the question.'))
+      ->setDisplayOptions('form', [
+        'type' => 'text_textarea',
+        'weight' => 10,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('view', [
+        'type' => 'text_default',
+        'label' => 'above',
+        'weight' => 10,
+      ])
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['uid'] = BaseFieldDefinition::create('entity_reference')
+      ->setTranslatable(TRUE)
+      ->setLabel(t('Author'))
+      ->setDescription(t('The user ID of the question author.'))
+      ->setSetting('target_type', 'user')
+      ->setDisplayOptions('form', [
+        'type' => 'entity_reference_autocomplete',
+        'settings' => [
+          'match_operator' => 'CONTAINS',
+          'size' => 60,
+          'placeholder' => '',
+        ],
+        'weight' => 15,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('view', [
+        'label' => 'above',
+        'type' => 'author',
+        'weight' => 15,
+      ])
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['created'] = BaseFieldDefinition::create('created')
+      ->setLabel(t('Authored on'))
+      ->setTranslatable(TRUE)
+      ->setDescription(t('The time that the question was created.'))
+      ->setDisplayOptions('view', [
+        'label' => 'above',
+        'type' => 'timestamp',
+        'weight' => 20,
+      ])
+      ->setDisplayConfigurable('form', TRUE)
+      ->setDisplayOptions('form', [
+        'type' => 'datetime_timestamp',
+        'weight' => 20,
+      ])
+      ->setDisplayConfigurable('view', TRUE);
+
+    $fields['changed'] = BaseFieldDefinition::create('changed')
+      ->setLabel(t('Changed'))
+      ->setTranslatable(TRUE)
+      ->setDescription(t('The time that the question was last edited.'));
+
+    return $fields;
+  }
+
+}
