4a2bf30c319e79e170e4ab930d96dd4a1607b8b6
Simon Baese <simon@youvo.org>
2021-09-09 19:36:20 +0200

    WIP quiz form embedding question forms

--- web/modules/academy/quizzes/src/Form/ParagraphWithQuizForm.php
+++ web/modules/academy/quizzes/src/Form/ParagraphWithQuizForm.php
@@ -4,8 +4,11 @@ namespace Drupal\quizzes\Form;
 
 use Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException;
 use Drupal\Component\Plugin\Exception\PluginNotFoundException;
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\AppendCommand;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\paragraphs\Form\ParagraphForm;
+use Drupal\quizzes\Entity\Question;
 
 /**
  * Form controller for the paragraph entity with quiz edit forms.
@@ -36,10 +39,27 @@ class ParagraphWithQuizForm extends ParagraphForm {
         $form['add_' . $question_type->id()] = [
           '#type' => 'submit',
           '#value' => '+ ' . $question_type->label() . ' Question',
-          '#weight' => '100',
+          '#data' => $question_type->id(),
+          '#weight' => '99',
+          '#submit' => ['::rebuildForm'],
+          '#ajax' => [
+            'callback' => [$this, 'addQuestionToForm'],
+            'disable-refocus' => TRUE,
+            'event' => 'click',
+            'effect' => 'none',
+            'progress' => [
+              'type' => 'none',
+            ],
+          ],
         ];
-
       }
+
+      // We add a container here to append the question forms returned by the
+      // AJAX callback.
+      $form['question_container'] = [
+        '#type' => 'container',
+        '#weight' => '100',
+      ];
     }
 
     return $form;
@@ -51,6 +71,28 @@ class ParagraphWithQuizForm extends ParagraphForm {
   public function save(array $form, FormStateInterface $form_state) {
     // Save entity.
     $result = parent::save($form, $form_state);
+  }
+
+  /**
+   * Adds a question form to the quiz form.
+   */
+  public function addQuestionToForm(array &$form, FormStateInterface $form_state) {
+    $question_type = $form_state->getTriggeringElement()['#data'];
+
+    $question = Question::create(['bundle' => $question_type]);
+    $question_form = \Drupal::service('entity.form_builder')->getForm($question, 'edit');
+    unset($question_form['actions']);
+    $response = new AjaxResponse();
+    $response->addCommand(new AppendCommand('#edit-question-container', $question_form));
+
+    return $response;
+  }
+
+  /**
+   * We need to rebuild to update form_state.
+   */
+  public function rebuildForm(array &$form, FormStateInterface $form_state) {
+    $form_state->setRebuild(TRUE);
   }
 
 }
