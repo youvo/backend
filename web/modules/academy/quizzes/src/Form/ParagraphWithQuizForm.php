9119da6545d283352cefc6ebfa37a79558fb953c
Simon Baese <simon@youvo.org>
2021-09-09 17:35:24 +0200

    Add empty button wrappers to add questions to quiz

--- web/modules/academy/quizzes/src/Form/ParagraphWithQuizForm.php
+++ web/modules/academy/quizzes/src/Form/ParagraphWithQuizForm.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\quizzes\Form;
 
+use Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException;
+use Drupal\Component\Plugin\Exception\PluginNotFoundException;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\paragraphs\Form\ParagraphForm;
 
@@ -9,6 +11,39 @@ use Drupal\paragraphs\Form\ParagraphForm;
  * Form controller for the paragraph entity with quiz edit forms.
  */
 class ParagraphWithQuizForm extends ParagraphForm {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function form(array $form, FormStateInterface $form_state) {
+
+    $form = parent::form($form, $form_state);
+
+    if ($this->entity->bundle() === 'quiz') {
+
+      // We load the differen question types and append buttons to add a
+      // question to the current quiz.
+      $question_types = [];
+      try {
+        $question_types = \Drupal::entityTypeManager()
+          ->getStorage('question_type')
+          ->loadMultiple();
+      }
+      catch (InvalidPluginDefinitionException | PluginNotFoundException $e) {
+        watchdog_exception('Quiz', $e, 'Could not load quiz types in edit form.');
+      }
+      foreach ($question_types as $question_type) {
+        $form['add_' . $question_type->id()] = [
+          '#type' => 'submit',
+          '#value' => '+ ' . $question_type->label() . ' Question',
+          '#weight' => '100',
+        ];
+
+      }
+    }
+
+    return $form;
+  }
 
   /**
    * {@inheritdoc}
