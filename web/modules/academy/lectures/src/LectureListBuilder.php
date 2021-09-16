4da6bb0b74e0c39cea8093478166975bc61df3c4
Simon Baese <simon@youvo.org>
2021-09-16 19:23:24 +0200

    Make lecture entities sortable

--- web/modules/academy/lectures/src/LectureListBuilder.php
+++ web/modules/academy/lectures/src/LectureListBuilder.php
@@ -3,17 +3,26 @@
 namespace Drupal\lectures;
 
 use Drupal\Core\Entity\EntityInterface;
-use Drupal\Core\Entity\EntityListBuilder;
 use Drupal\Core\Datetime\DateFormatterInterface;
+use Drupal\Core\Entity\EntityListBuilder;
 use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Form\FormInterface;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Routing\RedirectDestinationInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a list controller for the lecture entity type.
  */
-class LectureListBuilder extends EntityListBuilder {
+class LectureListBuilder extends EntityListBuilder implements FormInterface {
+
+  /**
+   * The entities being listed.
+   *
+   * @var \Drupal\Core\Entity\EntityInterface[]
+   */
+  protected $entities = [];
 
   /**
    * The date formatter service.
@@ -28,6 +37,13 @@ class LectureListBuilder extends EntityListBuilder {
    * @var \Drupal\Core\Routing\RedirectDestinationInterface
    */
   protected $redirectDestination;
+
+  /**
+   * The form builder.
+   *
+   * @var \Drupal\Core\Form\FormBuilderInterface
+   */
+  protected $formBuilder;
 
   /**
    * Constructs a new LectureListBuilder object.
@@ -60,48 +76,115 @@ class LectureListBuilder extends EntityListBuilder {
   }
 
   /**
+   * Returns the form builder.
+   *
+   * @return \Drupal\Core\Form\FormBuilderInterface
+   *   The form builder.
+   */
+  protected function formBuilder() {
+    if (!$this->formBuilder) {
+      $this->formBuilder = \Drupal::formBuilder();
+    }
+    return $this->formBuilder;
+  }
+
+  /**
    * {@inheritdoc}
    */
   public function render() {
-    $build['table'] = parent::render();
+    return $this->formBuilder()->getForm($this);
+  }
 
-    $total = $this->getStorage()
-      ->getQuery()
-      ->count()
-      ->execute();
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'lecture_collection';
+  }
 
-    $build['summary']['#markup'] = $this->t('Total lectures: @total', ['@total' => $total]);
-    return $build;
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $form['mxdule'] = [
+      '#type' => 'details',
+      '#title' => $this->t('Module: Wurstsalat'),
+      '#description' => $this->t('Wurstsalat (German, literally sausage salad) is a tart sausage salad prepared with distilled white vinegar, oil and onions. A variation of the recipe adds strips of pickled gherkin. It is normally made from a sort of boiled sausage like Lyoner, stadtwurst, Regensburger Wurst (two types of cooked sausage) or extrawurst.'),
+      '#open' => FALSE,
+    ];
+
+    $form['mxdule']['entities'] = [
+      '#type' => 'table',
+      '#header' => $this->buildHeader(),
+      '#empty' => $this->t('There are no @label yet.', ['@label' => $this->entityType->getPluralLabel()]),
+      '#tabledrag' => [
+        [
+          'action' => 'order',
+          'relationship' => 'sibling',
+          'group' => 'weight',
+        ],
+      ],
+    ];
+
+    $this->entities = $this->load();
+    $delta = count($this->entities);
+
+    foreach ($this->entities as $entity) {
+      $row = $this->buildRow($entity);
+      if (isset($row['weight'])) {
+        $row['weight']['#delta'] = $delta;
+      }
+      $form['mxdule']['entities'][$entity->id()] = $row;
+    }
+
+    $form['actions']['#type'] = 'actions';
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Save'),
+      '#button_type' => 'primary',
+    ];
+
+    return $form;
   }
 
   /**
    * {@inheritdoc}
    */
   public function buildHeader() {
-    $header['id'] = $this->t('ID');
     $header['title'] = $this->t('Title');
     $header['status'] = $this->t('Status');
-    $header['uid'] = $this->t('Author');
-    $header['created'] = $this->t('Created');
-    $header['changed'] = $this->t('Updated');
-    return $header + parent::buildHeader();
+    $header = $header + parent::buildHeader();
+    $header['weight'] = $this->t('Weight');
+    return $header;
   }
 
   /**
    * {@inheritdoc}
    */
   public function buildRow(EntityInterface $entity) {
-    /** @var \Drupal\lectures\LectureInterface $entity */
-    $row['id'] = $entity->id();
-    $row['title'] = $entity->toLink();
-    $row['status'] = $entity->isEnabled() ? $this->t('Enabled') : $this->t('Disabled');
-    $row['uid']['data'] = [
-      '#theme' => 'username',
-      '#account' => $entity->getOwner(),
+    /** @var \Drupal\lectures\Entity\Lecture $entity */
+    // Override default values to markup elements.
+    $row['#attributes']['class'][] = 'draggable';
+    $row['#weight'] = $entity->get('weight')->value;
+    // Add content columns.
+    $row['title'] = [
+      '#markup' => $entity->getTitle(),
     ];
-    $row['created'] = $this->dateFormatter->format($entity->getCreatedTime());
-    $row['changed'] = $this->dateFormatter->format($entity->getChangedTime());
-    return $row + parent::buildRow($entity);
+    $row['status'] = [
+      '#markup' => $entity->isEnabled() ? $this->t('Enabled') : $this->t('Disabled'),
+    ];
+    // Contains operation column.
+    $row = $row + parent::buildRow($entity);
+    // Add weight column.
+    $row['weight'] = [
+      '#type' => 'weight',
+      '#title' => $this->t('Weight for @title', ['@title' => $entity->label()]),
+      '#title_display' => 'invisible',
+      '#default_value' => $entity->get('weight')->value,
+      '#attributes' => ['class' => ['weight']],
+    ];
+    return $row;
   }
 
   /**
@@ -114,6 +197,30 @@ class LectureListBuilder extends EntityListBuilder {
       $operations[$key]['query'] = $destination;
     }
     return $operations;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    // No validation.
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    foreach ($form_state->getValue('entities') as $id => $value) {
+      /** @var \Drupal\lectures\Entity\Lecture $lecture */
+      $lecture = $this->entities[$id];
+      if (isset($lecture) && $lecture->get('weight')->value != $value['weight']) {
+        // Save entity only when its weight was changed.
+        $lecture->set('weight', $value['weight']);
+        $lecture->save();
+      }
+    }
   }
 
 }
