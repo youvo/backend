d1046d0240ef4aa9264210ab793399a8488d4d69
Simon Baese <simon@youvo.org>
2021-09-27 18:37:59 +0200

    Refactor project module and distribute configuration files

---
+++ web/modules/projects/project/src/ParamConverter/ProjectParamConverter.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Drupal\project\ParamConverter;
+
+use Drupal\Component\Uuid\Uuid;
+use Drupal\Core\ParamConverter\EntityConverter;
+use Drupal\Core\ParamConverter\ParamConverterInterface;
+
+/**
+ * Parameter converter for replacing UUIDs with IDs.
+ *
+ * @see \Drupal\Core\ParamConverter\EntityConverter
+ *
+ * @todo Remove when https://www.drupal.org/node/2353611 lands.
+ */
+class ProjectParamConverter extends EntityConverter implements ParamConverterInterface {
+
+  /**
+   * {@inheritdoc}
+   *
+   * @throws \Drupal\Core\ParamConverter\ParamNotConvertedException
+   */
+  public function convert($value, $definition, $name, array $defaults) {
+
+    // Convert request with UUIDs to IDs by querying the database.
+    if ($this->isRestRequest($defaults) && Uuid::isValid($value)) {
+      $entity_type_id = $this->getEntityTypeFromDefaults($definition, $name, $defaults);
+      $query = \Drupal::entityQuery($entity_type_id)
+        ->condition('uuid', $value)
+        ->execute();
+      $value = reset($query);
+    }
+
+    return parent::convert($value, $definition, $name, $defaults);
+  }
+
+  /**
+   * Checks if the current route stems from a REST request.
+   *
+   * @param array $defaults
+   *   The route defaults array.
+   */
+  private function isRestRequest(array $defaults) {
+    $valid_methods = $defaults['_route_object']->getMethods();
+    $method = substr($defaults['_route'], strrpos($defaults['_route'], ".") + 1);
+    return in_array($method, $valid_methods);
+  }
+
+}
---
+++ web/modules/projects/project/src/Plugin/rest/resource/ProjectRestResourceRoutesTrait.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Drupal\project\Plugin\rest\resource;
+
+use Symfony\Component\Routing\Exception\InvalidParameterException;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Defines routes method for project rest resources.
+ *
+ * Note that the pluginId of the respective service has to match the routeId of
+ * the matching base route which defines the administration form.
+ */
+trait ProjectRestResourceRoutesTrait {
+
+  /**
+   * {@inheritdoc}
+   */
+  abstract public function getPluginId();
+
+  /**
+   * {@inheritdoc}
+   */
+  abstract public function availableMethods();
+
+  /**
+   * {@inheritdoc}
+   */
+  abstract public function getPluginDefinition();
+
+  /**
+   * {@inheritdoc}
+   */
+  abstract protected function getBaseRoute($canonical_path, $method);
+
+  /**
+   * {@inheritdoc}
+   */
+  public function routes() {
+    $collection = new RouteCollection();
+
+    $definition = $this->getPluginDefinition();
+    $canonical_path = $definition['uri_paths']['canonical'];
+    $route_name = strtr($this->getPluginId(), ':', '.');
+
+    // The base route is not available during installation. Route definition
+    // will be updated during first cache rebuild.
+    $base_route_array = \Drupal::service('router.route_provider')->getRoutesByNames([$route_name]);
+    $base_route = !empty($base_route_array) ? reset($base_route_array) : NULL;
+
+    $methods = $this->availableMethods();
+    foreach ($methods as $method) {
+      $route = $this->getBaseRoute($canonical_path, $method);
+
+      // Bequeath route definition from base route.
+      if ($base_route instanceof Route) {
+        if (!$this->baseRouteProper($base_route)) {
+          throw new InvalidParameterException('Transition path has to provide transition default, _custom_access requirement and entity:node parameters.');
+        }
+        $route->setDefault('transition', $base_route->getDefault('transition'));
+        $route->setRequirement('_custom_access', $base_route->getRequirement('_custom_access'));
+        $route->addOptions(['parameters' => $base_route->getOption('parameters')]);
+      }
+
+      // Appended method is crucial for recognition in parameter converter.
+      $collection->add("$route_name.$method", $route);
+    }
+
+    return $collection;
+  }
+
+  /**
+   * Checks if base route was properly defined in routing.yml.
+   *
+   * @param \Symfony\Component\Routing\Route $base_route
+   *   Base route of transition.
+   */
+  private function baseRouteProper(Route $base_route) {
+    if (empty($base_route->getDefault('transition')) ||
+      empty($base_route->getRequirement('_custom_access')) ||
+      empty($base_route->getOption('parameters'))) {
+      return FALSE;
+    }
+    return TRUE;
+  }
+
+}
