2a88e67d7d2d2ae0cef1f789f843a216666a1027
Simon Baese <simon@youvo.org>
2021-10-07 18:18:15 +0200

    Rework child entity route provider

--- web/modules/academy/child_entities/src/Routing/ChildContentEntityHtmlRouteProvider.php
+++ web/modules/academy/child_entities/src/Routing/ChildContentEntityHtmlRouteProvider.php
@@ -5,9 +5,7 @@ namespace Drupal\child_entities\Routing;
 use Drupal\child_entities\ChildEntityEnsureTrait;
 use Drupal\child_entities\Controller\ChildEntityController;
 use Drupal\Core\Entity\EntityTypeInterface;
-use Drupal\Core\Entity\Exception\UnsupportedEntityTypeDefinitionException;
 use Drupal\Core\Entity\Routing\AdminHtmlRouteProvider;
-use Symfony\Component\Routing\Route;
 
 /**
  * Provides routes for Child Entities.
@@ -35,65 +33,44 @@ class ChildContentEntityHtmlRouteProvider extends AdminHtmlRouteProvider {
    *
    * @throws \Drupal\Core\Entity\Exception\UnsupportedEntityTypeDefinitionException
    * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   *
+   * @todo Route path definition is manual at the moment. Rework maybe.
    */
   public function getRoutes(EntityTypeInterface $entity_type) {
     $this->entityImplementsChildEntityInterface($entity_type);
-
     $collection = parent::getRoutes($entity_type);
-    $parent_type = \Drupal::entityTypeManager()->getDefinition($entity_type->getKey('parent'));
-    if (!$parent_type->hasLinkTemplate('canonical') && !$parent_type->hasLinkTemplate('edit-form')) {
-      throw new UnsupportedEntityTypeDefinitionException('The parent entity type ' . $parent_type->id() . ' does not have a canonical or edit route.');
-    }
-
     foreach ($collection as $key => $route) {
       $option_parameters = $route->getOption('parameters');
       if (!is_array($option_parameters)) {
         $option_parameters = [];
       }
-      $option_parameters[$entity_type->getKey('parent')] = [
-        'type' => 'entity:' . $entity_type->getKey('parent'),
-      ];
+      $this->appendParentOptionParameters($option_parameters, $entity_type);
       $route->setOption('parameters', $option_parameters);
-      // @todo Route definition is manual at the moment. Rework maybe.
-      // $this->prepareWithParentEntities($route, $entity_type);
       $collection->add($key, $route);
     }
     return $collection;
   }
 
   /**
-   * Prepare the Entity with its Parents.
+   * Append option parameters with parent entities.
    *
-   * @param \Symfony\Component\Routing\Route $route
-   *   The Route.
+   * @param array $option_parameters
+   *   The option parameters.
    * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
    *   The Child Entity Type.
    *
    * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
    */
-  private function prepareWithParentEntities(Route $route, EntityTypeInterface $entity_type) {
-    $parent_type = \Drupal::entityTypeManager()->getDefinition($entity_type->getKey('parent'));
-
-    if ($parent_type->hasLinkTemplate('canonical')) {
-      $link = $parent_type->getLinkTemplate('canonical');
-    }
-    else {
-      $link = $parent_type->getLinkTemplate('edit-form');
-    }
-
-    $route->setPath($link . $route->getPath());
-
-    $option_parameters = $route->getOption('parameters');
-    if (!is_array($option_parameters)) {
-      $option_parameters = [];
-    }
+  private function appendParentOptionParameters(array &$option_parameters, EntityTypeInterface $entity_type) {
+    // Add entity option parameters.
     $option_parameters[$entity_type->getKey('parent')] = [
       'type' => 'entity:' . $entity_type->getKey('parent'),
     ];
-    $route->setOption('parameters', $option_parameters);
 
+    // If parent is another child append its parents.
+    $parent_type = \Drupal::entityTypeManager()->getDefinition($entity_type->getKey('parent'));
     if ($parent_type->hasKey('parent')) {
-      $this->prepareWithParentEntities($route, $parent_type);
+      $this->appendParentOptionParameters($option_parameters, $parent_type);
     }
   }
 
