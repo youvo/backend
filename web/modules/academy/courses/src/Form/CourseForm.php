14d095cc97dbc72322ee7dc75c009aa726b0d5eb
Simon Baese <simon@youvo.org>
2021-10-08 12:30:13 +0200

    Add throws tags in course form

--- web/modules/academy/courses/src/Form/CourseForm.php
+++ web/modules/academy/courses/src/Form/CourseForm.php
@@ -42,6 +42,9 @@ class CourseForm extends ContentEntityForm {
 
   /**
    * {@inheritdoc}
+   *
+   * @throws \Drupal\Core\Entity\EntityMalformedException
+   * @throws \Drupal\Core\Entity\EntityStorageException
    */
   public function save(array $form, FormStateInterface $form_state) {
 
