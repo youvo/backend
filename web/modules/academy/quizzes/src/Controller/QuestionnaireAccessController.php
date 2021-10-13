60b31c4b1a3e695b46fc2049a92dd59a18a555c4
Simon Baese <simon@youvo.org>
2021-10-13 18:45:34 +0200

    Prepare questionnaire rest resource

---
+++ web/modules/academy/quizzes/src/Controller/QuestionnaireAccessController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\quizzes\Controller;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\quizzes\Entity\Quiz;
+use Symfony\Component\Routing\Route;
+
+/**
+ * Access controller for transition forms.
+ */
+class QuestionnaireAccessController extends ControllerBase {
+
+  /**
+   * Checks access for questionnaire submission.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   Run access checks for this account.
+   * @param \Symfony\Component\Routing\Route $route
+   *   The requested route.
+   * @param \Drupal\quizzes\Entity\Quiz|null $questionnaire
+   *   The questionnaire entity.
+   *
+   * @return \Drupal\Core\Access\AccessResultInterface
+   *   The access results.
+   */
+  public function accessQuestionnaire(AccountInterface $account, Route $route, Quiz $questionnaire = NULL) {
+    if ($questionnaire instanceof Quiz) {
+      $methods = $route->getMethods();
+      return AccessResult::allowedIf($account->hasPermission('restful ' . strtolower(reset($methods)) . ' questionnaire:submission'));
+    }
+    return AccessResult::forbidden();
+  }
+
+}
