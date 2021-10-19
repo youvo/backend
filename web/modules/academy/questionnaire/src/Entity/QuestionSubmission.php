6aa86e1c0cf5eedec0e82a9dfdc54a7d45c6777a
Simon Baese <simon@youvo.org>
2021-10-19 16:49:32 +0200

    Add base definition for question submission entity

---
+++ web/modules/academy/questionnaire/src/Entity/QuestionSubmission.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace Drupal\questionnaire\Entity;
+
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityChangedTrait;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\user\UserInterface;
+
+/**
+ * Defines the question submission entity class.
+ *
+ * @ContentEntityType(
+ *   id = "question_submission",
+ *   label = @Translation("Question Submission"),
+ *   label_collection = @Translation("Question Submissions"),
+ *   base_table = "question_submissions",
+ *   admin_permission = "administer questions",
+ *   entity_keys = {
+ *     "id" = "sid",
+ *     "uuid" = "uuid",
+ *     "question" = "question",
+ *     "uid" = "uid"
+ *   }
+ * )
+ */
+class QuestionSubmission extends ContentEntityBase {
+
+  use EntityChangedTrait;
+
+  /**
+   * {@inheritdoc}
+   *
+   * When a new question submission entity is created, set the uid entity
+   * reference to the current user as the creator of the entity.
+   */
+  public static function preCreate(EntityStorageInterface $storage, array &$values) {
+    parent::preCreate($storage, $values);
+    $values += ['uid' => \Drupal::currentUser()->id()];
+  }
+
+  /**
+   * Get created time.
+   */
+  public function getCreatedTime() {
+    return $this->get('created')->value;
+  }
+
+  /**
+   * Set created time.
+   */
+  public function setCreatedTime($timestamp) {
+    $this->set('created', $timestamp);
+    return $this;
+  }
+
+  /**
+   * Get owner.
+   */
+  public function getOwner() {
+    return $this->get('uid')->entity;
+  }
+
+  /**
+   * Get owner ID.
+   */
+  public function getOwnerId() {
+    return $this->get('uid')->target_id;
+  }
+
+  /**
+   * Set ownder ID.
+   */
+  public function setOwnerId($uid) {
+    $this->set('uid', $uid);
+    return $this;
+  }
+
+  /**
+   * Set owner.
+   */
+  public function setOwner(UserInterface $account) {
+    $this->set('uid', $account->id());
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+
+    $fields = parent::baseFieldDefinitions($entity_type);
+
+    $fields['question'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Question'))
+      ->setDescription(t('The question ID of the respective submission.'))
+      ->setSetting('target_type', 'question')
+      ->setReadOnly(TRUE);
+
+    $fields['question'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Question'))
+      ->setDescription(t('The question ID of the respective submission.'))
+      ->setSetting('target_type', 'question')
+      ->setReadOnly(TRUE);
+
+    $fields['question_revision'] = BaseFieldDefinition::create('integer')
+      ->setLabel(t('Question revision ID'))
+      ->setReadOnly(TRUE)
+      ->setSetting('unsigned', TRUE);
+
+    $fields['uid'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Author'))
+      ->setDescription(t('The user ID of the question author.'))
+      ->setSetting('target_type', 'user');
+
+    $fields['value'] = BaseFieldDefinition::create('string_long')
+      ->setLabel(t('Submission value'))
+      ->setDescription(t('The value of the submission.'));
+
+    $fields['langcode'] = BaseFieldDefinition::create('language')
+      ->setLabel(t('Language'))
+      ->setDescription(t('The submission language code.'));
+
+    $fields['created'] = BaseFieldDefinition::create('created')
+      ->setLabel(t('Authored on'))
+      ->setDescription(t('The time that the submission was created.'));
+
+    $fields['changed'] = BaseFieldDefinition::create('changed')
+      ->setLabel(t('Changed'))
+      ->setDescription(t('The time that the submission was last edited.'));
+
+    return $fields;
+  }
+
+}
