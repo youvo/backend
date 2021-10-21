6a8e775021d7f763c1d58a34464f5acdd522cf06
Simon Baese <simon@youvo.org>
2021-10-21 16:32:08 +0200

    Decouple progress resources as reusable abstract class and add time injection

--- web/modules/academy/progress/src/Plugin/rest/resource/LectureCompleteResource.php
+++ web/modules/academy/progress/src/Plugin/rest/resource/LectureCompleteResource.php
@@ -2,20 +2,10 @@
 
 namespace Drupal\progress\Plugin\rest\resource;
 
-use Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException;
-use Drupal\Component\Plugin\Exception\PluginNotFoundException;
-use Drupal\Component\Serialization\Json;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Session\AccountInterface;
 use Drupal\lectures\Entity\Lecture;
-use Drupal\progress\Entity\LectureProgress;
-use Drupal\rest\Plugin\ResourceBase;
+use Drupal\rest\ModifiedResourceResponse;
 use Drupal\rest\ResourceResponse;
-use Psr\Log\LoggerInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Exception\HttpException;
-use Symfony\Component\Routing\RouteCollection;
 
 /**
  * Provides Progress Lecture Complete Resource.
@@ -28,71 +18,7 @@ use Symfony\Component\Routing\RouteCollection;
  *   }
  * )
  */
-class LectureCompleteResource extends ResourceBase {
-
-  /**
-   * The current user.
-   *
-   * @var \Drupal\Core\Session\AccountInterface
-   */
-  protected $currentUser;
-
-  /**
-   * The entity type manager.
-   *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
-   */
-  protected $entityTypeManager;
-
-  /**
-   * The serialization by Json service.
-   *
-   * @var \Drupal\Component\Serialization\Json
-   */
-  protected $serializationJson;
-
-  /**
-   * Constructs a QuestionSubmissionResource object.
-   *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin_id for the plugin instance.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param array $serializer_formats
-   *   The available serialization formats.
-   * @param \Psr\Log\LoggerInterface $logger
-   *   A logger instance.
-   * @param \Drupal\Core\Session\AccountInterface $current_user
-   *   The current user.
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager.
-   * @param \Drupal\Component\Serialization\Json $serialization_json
-   *   The serialization by Json service.
-   */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, array $serializer_formats, LoggerInterface $logger, AccountInterface $current_user, EntityTypeManagerInterface $entity_type_manager, Json $serialization_json) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
-    $this->currentUser = $current_user;
-    $this->entityTypeManager = $entity_type_manager;
-    $this->serializationJson = $serialization_json;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->getParameter('serializer.formats'),
-      $container->get('logger.factory')->get('rest'),
-      $container->get('current_user'),
-      $container->get('entity_type.manager'),
-      $container->get('serialization.json')
-    );
-  }
+class LectureCompleteResource extends LectureProgressResource {
 
   /**
    * Responds GET requests.
@@ -100,23 +26,32 @@ class LectureCompleteResource extends ResourceBase {
    * @param \Drupal\lectures\Entity\Lecture $lecture
    *   The referenced lecture.
    *
-   * @return \Drupal\rest\ResourceResponse
+   * @return \Drupal\rest\ResourceResponse|ModifiedResourceResponse
    *   Response.
    */
   public function get(Lecture $lecture) {
 
-    // Fetch questions and answers.
-    $submission['type'] = $lecture->bundle();
-    $submission['uuid'] = $lecture->uuid();
+    // Get the respective lecture progress by lecture and current user.
+    $progress = $this->getRespectiveLectureProgress($lecture);
+
+    // There is no progress for this lecture by this user.
+    if (empty($progress)) {
+      return new ModifiedResourceResponse(NULL, 204);
+    }
+
+    // Fetch progress information.
+    $data['enrolled'] = $progress->getEnrollmentTime();
+    $data['accessed'] = $progress->getAccessTime();
+    $data['completed'] = $progress->getCompletedTime();
 
     // Compile response with structured data.
     $response = new ResourceResponse([
       'type' => 'progress.lecture.complete.resource',
-      'data' => $submission,
+      'data' => $data,
     ]);
 
     // Add cacheable dependency to refresh response when lecture is udpated.
-    $response->addCacheableDependency($lecture);
+    $response->addCacheableDependency($progress);
 
     return $response;
   }
@@ -139,39 +74,6 @@ class LectureCompleteResource extends ResourceBase {
       ->decode($request->getContent());
 
     return new ResourceResponse('Hello POST.', 200);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function routes() {
-
-    // Gather properties.
-    $collection = new RouteCollection();
-
-    $definition = $this->getPluginDefinition();
-    $canonical_path = $definition['uri_paths']['canonical'];
-    $create_path = $definition['uri_paths']['create'];
-    $route_name = strtr($this->pluginId, ':', '.');
-
-    // Add access check and route entity context parameter for each method.
-    foreach ($this->availableMethods() as $method) {
-      $route = $this->getBaseRoute($canonical_path, $method);
-      $route->setRequirement('_custom_access', '\Drupal\progress\Controller\LectureProgressAccessController::accessLecture');
-
-      // Add route entity context parameters.
-      $parameters = $route->getOption('parameters') ?: [];
-      $route->setOption('parameters', $parameters + [
-        'lecture' => [
-          'type' => 'entity:lecture',
-          'converter' => 'paramconverter.uuid',
-        ],
-      ]);
-
-      $collection->add("$route_name.$method", $route);
-    }
-
-    return $collection;
   }
 
 }
