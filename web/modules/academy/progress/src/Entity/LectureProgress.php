36bd61deb9de1bbb89a1e0b5be55d86445fd19b2
Simon Baese <simon@youvo.org>
2021-10-21 14:06:32 +0200

    Introduce academy progress module with LectureProgress entity

---
+++ web/modules/academy/progress/src/Entity/LectureProgress.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Drupal\progress\Entity;
+
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityChangedTrait;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\progress\LectureProgressInterface;
+
+/**
+ * Defines the LectureProgress entity class.
+ *
+ * @ContentEntityType(
+ *   id = "lecture_progress",
+ *   label = @Translation("LectureProgress"),
+ *   label_collection = @Translation("LectureProgress"),
+ *   base_table = "lecture_progress",
+ *   admin_permission = "administer progress",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "uuid" = "uuid",
+ *     "lecture" = "lecture",
+ *     "uid" = "uid"
+ *   }
+ * )
+ */
+class LectureProgress extends ContentEntityBase implements LectureProgressInterface {
+
+  use EntityChangedTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getEnrollmentTime() {
+    return $this->get('enrolled')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getAccessTime() {
+    return $this->get('access')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setAccessTime(int $timestamp): LectureProgress {
+    $this->set('access', $timestamp);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCompletedTime() {
+    return $this->get('completed')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setCompletedTime(int $timestamp): LectureProgress {
+    $this->set('completed', $timestamp);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getOwner() {
+    return $this->get('uid')->entity;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getOwnerId() {
+    return $this->get('uid')->target_id;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+
+    $fields = parent::baseFieldDefinitions($entity_type);
+
+    $fields['lecture'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Lecture'))
+      ->setDescription(t('The lecture ID.'))
+      ->setSetting('target_type', 'lecture')
+      ->setRequired(TRUE)
+      ->setReadOnly(TRUE);
+
+    $fields['uid'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Author'))
+      ->setDescription(t('The user ID of the question author.'))
+      ->setSetting('target_type', 'user')
+      ->setRequired(TRUE);
+
+    $fields['langcode'] = BaseFieldDefinition::create('language')
+      ->setLabel(t('Language'))
+      ->setDescription(t('The lecture language code on enrollment.'))
+      ->setRequired(TRUE);
+
+    $fields['enrolled'] = BaseFieldDefinition::create('enrolled')
+      ->setLabel(t('Initial access (enrollment)'))
+      ->setDescription(t('The time that the lecture was accessed initially.'))
+      ->setRequired(TRUE)
+      ->setReadOnly(TRUE);
+
+    $fields['access'] = BaseFieldDefinition::create('access')
+      ->setLabel(t('Last access'))
+      ->setDescription(t('The time that the lecture was last accessed.'))
+      ->setRequired(TRUE);
+
+    $fields['completed'] = BaseFieldDefinition::create('completed')
+      ->setLabel(t('Completed'))
+      ->setDescription(t('The time that the lecture was completed.'))
+      ->setDefaultValue(0);
+
+    return $fields;
+  }
+
+}
---
+++ web/modules/academy/progress/src/LectureProgressInterface.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Drupal\progress;
+
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\EntityChangedInterface;
+
+/**
+ * Provides an interface defining a LectureProgress entity type.
+ */
+interface LectureProgressInterface extends ContentEntityInterface, EntityChangedInterface {
+
+  /**
+   * Gets the Lecture enrollment timestamp.
+   *
+   * @return int
+   *   Enrollment timestamp of the Lecture.
+   */
+  public function getEnrollmentTime();
+
+  /**
+   * Gets the Lecture access timestamp.
+   *
+   * @return int
+   *   Access timestamp of the Lecture.
+   */
+  public function getAccessTime();
+
+  /**
+   * Sets the Lecture access timestamp.
+   *
+   * @param int $timestamp
+   *   The Lecture access timestamp.
+   *
+   * @return \Drupal\progress\LectureProgressInterface
+   *   The called LectureProgress entity.
+   */
+  public function setAccessTime(int $timestamp);
+
+  /**
+   * Gets the Lecture completed timestamp.
+   *
+   * @return int
+   *   Completed timestamp of the Lecture.
+   */
+  public function getCompletedTime();
+
+  /**
+   * Sets the Lecture completed timestamp.
+   *
+   * @param int $timestamp
+   *   The Lecture completed timestamp.
+   *
+   * @return \Drupal\progress\LectureProgressInterface
+   *   The called LectureProgress entity.
+   */
+  public function setCompletedTime(int $timestamp);
+
+  /**
+   * Returns the entity owner's user entity.
+   *
+   * @return \Drupal\user\UserInterface
+   *   The owner user entity.
+   */
+  public function getOwner();
+
+  /**
+   * Returns the entity owner's user ID.
+   *
+   * @return int|null
+   *   The owner user ID, or NULL in case the user ID field has not been set on
+   *   the entity.
+   */
+  public function getOwnerId();
+
+}
