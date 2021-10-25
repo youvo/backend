3ecf5f880418b64c37b4965baf865588d8c5b8f9
Simon Baese <simon@youvo.org>
2021-10-25 20:04:12 +0200

    Introduce LectureProgressManager for unified progress handling

--- web/modules/academy/progress/src/Plugin/rest/resource/LectureProgressResource.php
+++ web/modules/academy/progress/src/Plugin/rest/resource/LectureProgressResource.php
@@ -2,18 +2,14 @@
 
 namespace Drupal\progress\Plugin\rest\resource;
 
-use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException;
 use Drupal\Component\Plugin\Exception\PluginNotFoundException;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\Entity\EntityMalformedException;
 use Drupal\lectures\Entity\Lecture;
-use Drupal\progress\Entity\LectureProgress;
+use Drupal\progress\LectureProgressManager;
 use Drupal\rest\ModifiedResourceResponse;
 use Drupal\rest\Plugin\ResourceBase;
 use Drupal\rest\ResourceResponse;
-use Psr\Log\LoggerInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\RouteCollection;
 
@@ -21,70 +17,6 @@ use Symfony\Component\Routing\RouteCollection;
  * Provides Abstract for Lecture Progress Resources.
  */
 abstract class LectureProgressResource extends ResourceBase {
-
-  /**
-   * The current user.
-   *
-   * @var \Drupal\Core\Session\AccountInterface
-   */
-  protected $currentUser;
-
-  /**
-   * The entity type manager.
-   *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
-   */
-  protected $entityTypeManager;
-
-  /**
-   * The time service.
-   *
-   * @var \Drupal\Component\Datetime\TimeInterface
-   */
-  protected $time;
-
-  /**
-   * Constructs a QuestionSubmissionResource object.
-   *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin_id for the plugin instance.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param array $serializer_formats
-   *   The available serialization formats.
-   * @param \Psr\Log\LoggerInterface $logger
-   *   A logger instance.
-   * @param \Drupal\Core\Session\AccountInterface $current_user
-   *   The current user.
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager.
-   * @param \Drupal\Component\Datetime\TimeInterface $time
-   *   The time service.
-   */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, array $serializer_formats, LoggerInterface $logger, AccountInterface $current_user, EntityTypeManagerInterface $entity_type_manager, TimeInterface $time) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
-    $this->currentUser = $current_user;
-    $this->entityTypeManager = $entity_type_manager;
-    $this->time = $time;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->getParameter('serializer.formats'),
-      $container->get('logger.factory')->get('rest'),
-      $container->get('current_user'),
-      $container->get('entity_type.manager'),
-      $container->get('datetime.time')
-    );
-  }
 
   /**
    * Responds GET requests.
@@ -98,7 +30,16 @@ abstract class LectureProgressResource extends ResourceBase {
   public function get(Lecture $lecture) {
 
     // Get the respective lecture progress by lecture and current user.
-    $progress = $this->getRespectiveLectureProgress($lecture);
+    try {
+      $progress_manager = LectureProgressManager::create($lecture);
+      $progress = $progress_manager->getLectureProgress();
+    }
+    catch (InvalidPluginDefinitionException | PluginNotFoundException $e) {
+      throw new HttpException(500, 'Internal Server Error', $e);
+    }
+    catch (EntityMalformedException $e) {
+      throw new HttpException(417, 'The progress of the requested lecture has inconsistent persistent data.', $e);
+    }
 
     // There is no progress for this lecture by this user.
     if (empty($progress)) {
@@ -152,54 +93,6 @@ abstract class LectureProgressResource extends ResourceBase {
     }
 
     return $collection;
-  }
-
-  /**
-   * Gets the respective progress of the lecture by the current user.
-   *
-   * @param \Drupal\lectures\Entity\Lecture $lecture
-   *   The requested lecture.
-   *
-   * @returns \Drupal\progress\Entity\LectureProgress|null
-   *   The respective progress or NULL if no storage.
-   *
-   * @throws \Symfony\Component\HttpKernel\Exception\HttpException
-   */
-  protected function getRespectiveLectureProgress(Lecture $lecture): ?LectureProgress {
-    try {
-      // Get referenced LectureProgress.
-      $query = $this->entityTypeManager
-        ->getStorage('lecture_progress')
-        ->getQuery();
-      $progress_id = $query->condition('lecture', $lecture->id())
-        ->condition('uid', $this->currentUser->id())
-        ->execute();
-    }
-    catch (InvalidPluginDefinitionException | PluginNotFoundException $e) {
-      throw new HttpException(500, 'Internal Server Error', $e);
-    }
-
-    // Return nothing if there is no progress.
-    if (empty($progress_id)) {
-      return NULL;
-    }
-
-    // Something went wrong here.
-    if (count($progress_id) > 1) {
-      throw new HttpException(417, 'The progress of the requested lecture has inconsistent persistent data.');
-    }
-
-    try {
-      // Return loaded progress.
-      /** @var \Drupal\progress\Entity\LectureProgress $progress */
-      $progress = $this->entityTypeManager
-        ->getStorage('lecture_progress')
-        ->load(reset($progress_id));
-      return $progress;
-    }
-    catch (InvalidPluginDefinitionException | PluginNotFoundException $e) {
-      throw new HttpException(500, 'Internal Server Error', $e);
-    }
   }
 
 }
