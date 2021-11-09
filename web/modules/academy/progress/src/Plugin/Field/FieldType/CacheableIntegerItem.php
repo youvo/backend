eb3f4a5bda62d320c8386291b28231a0c603edb5
Simon Baese <simon@youvo.org>
2021-11-09 11:01:11 +0100

    Introduce cacheable integer field type

---
+++ web/modules/academy/progress/src/Plugin/Field/FieldType/CacheableIntegerItem.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Drupal\progress\Plugin\Field\FieldType;
+
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\Core\Field\Plugin\Field\FieldType\IntegerItem;
+use Drupal\Core\TypedData\DataDefinition;
+
+/**
+ * Defines the 'integer' entity field type with cacheability metadata.
+ *
+ * @FieldType(
+ *   id = "cacheable_integer_item",
+ *   label = @Translation("Cacheable Integer Item"),
+ *   description = @Translation("A field containing a integer value and cacheability metadata."),
+ *   no_ui = TRUE,
+ *   default_widget = "number",
+ *   default_formatter = "number_integer"
+ * )
+ */
+class CacheableIntegerItem extends IntegerItem {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
+    $properties['value'] = DataDefinition::create('cacheable_integer')
+      ->setLabel(t('Cacheable integer value'))
+      ->setRequired(TRUE);
+    return $properties;
+  }
+
+}
