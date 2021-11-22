689029eefaf60a13bf617ac50bc2cd3705797a87
Simon Baese <simon@youvo.org>
2021-11-22 21:53:35 +0100

    Append variables in exception on dashboard

--- web/modules/youvo/youvo/src/Controller/DashboardController.php
+++ web/modules/youvo/youvo/src/Controller/DashboardController.php
@@ -5,6 +5,7 @@ namespace Drupal\youvo\Controller;
 use Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException;
 use Drupal\Component\Plugin\Exception\PluginNotFoundException;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Utility\Error;
 
 /**
  * Controller for youvo_work landing pages.
@@ -26,9 +27,11 @@ class DashboardController extends ControllerBase {
         ->load($this->currentUser()->id());
       $page['fullname'] = $current_user->get('fullname')->value;
     }
-    catch (InvalidPluginDefinitionException | PluginNotFoundException) {
+    catch (InvalidPluginDefinitionException | PluginNotFoundException $e) {
+      $variables = Error::decodeException($e);
+      $variables['%id'] = $this->currentUser()->id();
       $this->loggerFactory->get('youvo')
-        ->error('Could not load user on dashboard.');
+        ->error('Could not load user with ID %id on dashboard.', $variables);
       $page['fullname'] = $this->t('User');
     }
 
