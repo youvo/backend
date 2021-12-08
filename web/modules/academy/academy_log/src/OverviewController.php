714af196c384f7c2a5f7a82d9b1534bcee0ba26e
Simon Baese <simon@youvo.org>
2021-12-08 14:16:55 +0100

    Simple overview page for course progression of academy participants

--- web/modules/academy/academy_log/src/OverviewController.php
+++ web/modules/academy/academy_log/src/OverviewController.php
@@ -3,6 +3,9 @@
 namespace Drupal\academy_log;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Datetime\DateFormatter;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\courses\Entity\Course;
 use Drupal\progress\ProgressManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -19,13 +22,23 @@ class OverviewController extends ControllerBase {
   private ProgressManager $progressManager;
 
   /**
+   * The date formatter service.
+   *
+   * @var \Drupal\Core\Datetime\DateFormatter
+   */
+  private DateFormatter $dateFormatter;
+
+  /**
    * Construct overview controller with services.
    *
    * @param \Drupal\progress\ProgressManager $progress_manager
    *   The progress manager service.
+   * @param \Drupal\Core\Datetime\DateFormatter $date_formatter
+   *   The date formatter service.
    */
-  public function __construct(ProgressManager $progress_manager) {
+  public function __construct(ProgressManager $progress_manager, DateFormatter $date_formatter) {
     $this->progressManager = $progress_manager;
+    $this->dateFormatter = $date_formatter;
   }
 
   /**
@@ -34,6 +47,7 @@ class OverviewController extends ControllerBase {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('progress.manager'),
+      $container->get('date.formatter')
     );
   }
 
@@ -44,7 +58,47 @@ class OverviewController extends ControllerBase {
 
     // Initialize.
     $page = [];
+    $accounts = $this->getCreativeAccounts();
+    $courses = $this->getAllCourses();
 
+    foreach ($accounts as $account) {
+      $sheet = [];
+      $sheet['name'] = $account->get('fullname')->value;
+      $sheet['progression'] = $this->calculateProgressionCourses($account);
+      $sheet['courses'] = [];
+      foreach ($courses as $course) {
+        $slip = [];
+        $progress = $this->progressManager->loadProgress($course, $account);
+        if ($course->id() == 1 && !isset($progress)) {
+          break;
+        }
+        $slip['title'] = $course->getTitle();
+        $slip['progression'] = isset($progress) ? $this->calculateProgressionLectures($course, $account) : 0;
+        $slip['enrolled'] = isset($progress) ? $this->dateFormatter->format($progress->getEnrollmentTime(), 'short') : '';
+        $slip['accessed'] = isset($progress) ? $this->dateFormatter->format($progress->getAccessTime(), 'short') : '';
+        $completed = $progress?->getCompletedTime();
+        $slip['completed'] = isset($completed) ? $this->dateFormatter->format($completed, 'short') : '';
+        $sheet['courses'][$course->id()] = $slip;
+        if (!isset($completed) || $completed == 0) {
+          break;
+        }
+      }
+      if (empty($sheet['courses'])) {
+        continue;
+      }
+      $page['participants'][$account->id()] = $sheet;
+    }
+
+    return [
+      '#theme' => 'overview',
+      '#page' => $page,
+    ];
+  }
+
+  /**
+   * Gets creative accounts.
+   */
+  private function getCreativeAccounts() {
     // Get all creatives, that are active and not associates.
     $associates_ids = [1, 14, 130, 136, 616, 621, 1200, 1888, 1889, 5124, 15970];
     $storage = $this->entityTypeManager()->getStorage('user');
@@ -53,17 +107,62 @@ class OverviewController extends ControllerBase {
       ->condition('roles', 'creative')
       ->condition('uid', $associates_ids, 'NOT IN')
       ->execute();
-    /** @var \Drupal\user\Entity\User[] $accounts */
+    /** @var \Drupal\user\UserInterface[] $accounts */
     $accounts = $storage->loadMultiple($uids);
+    return $accounts;
+  }
 
-    foreach ($accounts as $account) {
-      $page['participants'][$account->id()] = $account->get('fullname')->value;
+  /**
+   * Gets creative accounts.
+   */
+  private function getAllCourses() {
+    // Get all creatives, that are active and not associates.
+    $storage = $this->entityTypeManager()->getStorage('course');
+    $cids = $storage->getQuery()
+      ->condition('status', '1')
+      ->sort('weight')
+      ->execute();
+    /** @var \Drupal\courses\Entity\Course[] $courses */
+    $courses = $storage->loadMultiple($cids);
+    return $courses;
+  }
+
+  /**
+   * Returns progress in percent for course.
+   */
+  private function calculateProgressionLectures(Course $course, AccountInterface $account): int {
+
+    // Get lectures by completed for this course and account.
+    $lectures = $this->progressManager->getReferencedLecturesByCompleted($course, $account);
+
+    // If there are no lectures, there is no progress.
+    if (empty($lectures)) {
+      return 0;
     }
 
-    return [
-      '#theme' => 'overview',
-      '#page' => $page,
-    ];
+    // Calculate percentage.
+    $total = count($lectures);
+    $completed = count(array_filter($lectures, fn($l) => $l->completed));
+    return ceil($completed / $total * 100);
+  }
+
+  /**
+   * Returns progress in percent for curriculum.
+   */
+  private function calculateProgressionCourses(AccountInterface $account): int {
+
+    // Get courses by completed for this account.
+    $courses = $this->progressManager->getCoursesByCompleted($account);
+
+    // If there are no courses, there is no progress.
+    if (empty($courses)) {
+      return 0;
+    }
+
+    // Calculate percentage.
+    $total = count($courses);
+    $completed = count(array_filter($courses, fn($c) => $c->completed));
+    return ceil($completed / $total * 100);
   }
 
 }
