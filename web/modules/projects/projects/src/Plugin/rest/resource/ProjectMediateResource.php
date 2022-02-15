bb200b4f005bc5c69e84391b3995accafb623eef
Simon Baese <simon@youvo.org>
2022-02-15 17:13:30 +0100

    Rename participant in selection to creative

--- web/modules/projects/projects/src/Plugin/rest/resource/ProjectMediateResource.php
+++ web/modules/projects/projects/src/Plugin/rest/resource/ProjectMediateResource.php
@@ -55,7 +55,7 @@ class ProjectMediateResource extends ResourceBase {
         'applicants' => $applicants,
       ],
       'patch_required' => [
-        'selected_participants' => 'Array of uuid\'s of participants.',
+        'selected_creatives' => 'Array of uuid\'s of creatives.',
       ],
     ]);
 
@@ -84,43 +84,43 @@ class ProjectMediateResource extends ResourceBase {
     // Decode content of the request.
     $request_content = \Drupal::service('serialization.json')->decode($request->getContent());
 
-    // The selected_participants are required to process the request.
-    if (!array_key_exists('selected_participants', $request_content)) {
-      throw new BadRequestHttpException('Request body does not specify selected_participants.');
+    // The selected_creatives are required to process the request.
+    if (!array_key_exists('selected_creatives', $request_content)) {
+      throw new BadRequestHttpException('Request body does not specify selected_creatives.');
     }
 
-    // Set preliminary selected_participants variable.
-    $selected_participants = array_unique($request_content['selected_participants']);
+    // Set preliminary selected_creatives variable.
+    $selected_creatives = array_unique($request_content['selected_creatives']);
 
-    // Force at least one selected participant.
-    if (empty($selected_participants)) {
-      throw new BadRequestHttpException('The selected_participants array in the request body is empty.');
+    // Force at least one selected creative.
+    if (empty($selected_creatives)) {
+      throw new BadRequestHttpException('The selected_creatives array in the request body is empty.');
     }
 
-    // The selected_participants is expected to be delivered as a simple array.
-    if (count(array_filter(array_keys($selected_participants), 'is_string')) > 0) {
-      throw new BadRequestHttpException('The selected_participants array in the request body is malformed.');
+    // The selected_creatives is expected to be delivered as a simple array.
+    if (count(array_filter(array_keys($selected_creatives), 'is_string')) > 0) {
+      throw new BadRequestHttpException('The selected_creatives array in the request body is malformed.');
     }
 
-    // The entries of the selected participants array are expected to be UUIDs.
-    if (count(array_filter($selected_participants,
-        ['Drupal\Component\Uuid\Uuid', 'isValid'])) != count($selected_participants)) {
-      throw new BadRequestHttpException('The entries of the selected_participants array are not valid UUIDs.');
+    // The entries of the selected creatives array are expected to be UUIDs.
+    if (count(array_filter($selected_creatives,
+        ['Drupal\Component\Uuid\Uuid', 'isValid'])) != count($selected_creatives)) {
+      throw new BadRequestHttpException('The entries of the selected_creatives array are not valid UUIDs.');
     }
 
-    // Get applicants for current project and check if selected_participants are
+    // Get applicants for current project and check if selected_creatives are
     // applicable.
     $applicants = array_unique(array_keys($project->getApplicantsAsArray(TRUE)));
-    if (count(array_intersect($selected_participants, $applicants)) != count($selected_participants)) {
-      throw new UnprocessableEntityHttpException('Some selected participants did not apply for this project.');
+    if (count(array_intersect($selected_creatives, $applicants)) != count($selected_creatives)) {
+      throw new UnprocessableEntityHttpException('Some selected creatives did not apply for this project.');
     }
 
     // Now we are finally sure to mediate the project. We get the UIDs by query.
-    $selected_participants_ids = \Drupal::entityQuery('user')
-      ->condition('uuid', $selected_participants, 'IN')
+    $selected_creatives_ids = \Drupal::entityQuery('user')
+      ->condition('uuid', $selected_creatives, 'IN')
       ->execute();
-    if (!empty($selected_participants_ids) && $project->transitionMediate()) {
-      $project->setParticipants($selected_participants_ids, TRUE);
+    if (!empty($selected_creatives_ids) && $project->transitionMediate()) {
+      $project->setParticipants($selected_creatives_ids, TRUE);
       return new ResourceResponse('Project was mediated successfully.');
     }
 
