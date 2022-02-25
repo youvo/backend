7cf3340f74155cc21d06b97a95c9db3954a1df13
Simon Baese <simon@youvo.org>
2022-02-25 14:18:52 +0100

    Use dependency injection for project mediate resource

--- web/modules/projects/projects/src/Plugin/rest/resource/ProjectMediateResource.php
+++ web/modules/projects/projects/src/Plugin/rest/resource/ProjectMediateResource.php
@@ -2,9 +2,12 @@
 
 namespace Drupal\projects\Plugin\rest\resource;
 
+use Drupal\Component\Serialization\SerializationInterface;
 use Drupal\rest\Plugin\ResourceBase;
 use Drupal\rest\ResourceResponse;
 use Drupal\projects\ProjectInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
@@ -23,6 +26,48 @@ use Symfony\Component\HttpKernel\Exception\UnprocessableEntityHttpException;
 class ProjectMediateResource extends ResourceBase {
 
   use ProjectRestResourceRoutesTrait;
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Component\Serialization\SerializationInterface
+   */
+  protected $serializationJson;
+
+  /**
+   * Constructs a Drupal\rest\Plugin\ResourceBase object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param array $serializer_formats
+   *   The available serialization formats.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   A logger instance.
+   * @param \Drupal\Component\Serialization\SerializationInterface $serialization_json
+   *   Serialization with Json.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, array $serializer_formats, LoggerInterface $logger, SerializationInterface $serialization_json) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition, $serializer_formats, $logger);
+    $this->serializationJson = $serialization_json;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->getParameter('serializer.formats'),
+      $container->get('logger.factory')->get('rest'),
+      $container->get('serialization.json')
+    );
+  }
 
   /**
    * Responds GET requests.
@@ -83,7 +128,7 @@ class ProjectMediateResource extends ResourceBase {
   public function patch(ProjectInterface $project, Request $request) {
 
     // Decode content of the request.
-    $request_content = \Drupal::service('serialization.json')->decode($request->getContent());
+    $request_content = $this->serializationJson->decode($request->getContent());
 
     // The selected_creatives are required to process the request.
     if (!array_key_exists('selected_creatives', $request_content)) {
