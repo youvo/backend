358345f99233aba1ebcaadbdeb6aedba3a03eec2
Simon Baese <simon@youvo.org>
2022-03-01 07:59:20 +0900

    Add proposal entity shell

---
+++ web/modules/projects/proposals/src/Entity/Proposal.php
@@ -0,0 +1,213 @@
+<?php
+
+namespace Drupal\proposals\Entity;
+
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityChangedTrait;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+use Drupal\proposals\ProposalInterface;
+use Drupal\user\UserInterface;
+
+/**
+ * Defines the course entity class.
+ *
+ * @ContentEntityType(
+ *   id = "proposal",
+ *   label = @Translation("Proposal"),
+ *   label_collection = @Translation("Proposals"),
+ *   handlers = {
+ *     "access" = "Drupal\proposals\ProposalAccessControlHandler",
+ *     "form" = {
+ *       "add" = "Drupal\proposals\Form\ProposalForm",
+ *       "edit" = "Drupal\proposals\Form\ProposalForm",
+ *       "delete" = "Drupal\Core\Entity\ContentEntityDeleteForm"
+ *     },
+ *     "route_provider" = {
+ *       "html" = "Drupal\Core\Entity\Routing\AdminHtmlRouteProvider",
+ *     }
+ *   },
+ *   base_table = "proposal",
+ *   data_table = "proposal_field_data",
+ *   translatable = TRUE,
+ *   admin_permission = "administer proposals",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "langcode" = "langcode",
+ *     "label" = "title",
+ *     "uuid" = "uuid"
+ *   },
+ *   links = {
+ *     "canonical" = "/proposal/{proposal}
+ *     "add-form" = "/proposal/add",
+ *     "edit-form" = "/proposal/{proposal}/edit",
+ *     "delete-form" = "/proposal/{proposal}/delete",
+ *   },
+ * )
+ */
+class Proposal extends ContentEntityBase implements ProposalInterface {
+
+  use EntityChangedTrait;
+
+  /**
+   * {@inheritdoc}
+   *
+   * When a new proposal entity is created, set the uid entity reference to
+   * the current user as the creator of the entity.
+   */
+  public static function preCreate(EntityStorageInterface $storage, array &$values) {
+    parent::preCreate($storage, $values);
+    if (!isset($values['uid'])) {
+      $values['uid'] = \Drupal::currentUser()->id();
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getTitle() {
+    return $this->get('title')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setTitle($title) {
+    $this->set('title', $title);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isEnabled() {
+    return (bool) $this->get('status')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setStatus($status) {
+    $this->set('status', $status);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getCreatedTime() {
+    return $this->get('created')->value;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setCreatedTime($timestamp) {
+    $this->set('created', $timestamp);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getOwner() {
+    return $this->get('uid')->entity;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getOwnerId() {
+    return $this->get('uid')->target_id;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setOwnerId($uid) {
+    $this->set('uid', $uid);
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setOwner(UserInterface $account) {
+    $this->set('uid', $account->id());
+    return $this;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+
+    $fields = parent::baseFieldDefinitions($entity_type);
+
+    $fields['proposal'] = BaseFieldDefinition::create('string_long')
+      ->setTranslatable(TRUE)
+      ->setLabel(t('Proposal'))
+      ->setDescription(t('The proposal text.'))
+      ->setDisplayOptions('form', [
+        'type' => 'string_textarea',
+        'weight' => 10,
+      ]);
+
+    $fields['count_full_time'] = BaseFieldDefinition::create('string')
+      ->setTranslatable(TRUE)
+      ->setLabel(t('Count Full-time'))
+      ->setDescription(t('The number of full-time employees.'))
+      ->setSetting('max_length', 255)
+      ->setDisplayOptions('form', [
+        'type' => 'string_textfield',
+        'weight' => -3,
+      ]);
+
+    $fields['count_volunteers'] = BaseFieldDefinition::create('string')
+      ->setTranslatable(TRUE)
+      ->setLabel(t('Count Volunteers'))
+      ->setDescription(t('The number of volunteers.'))
+      ->setSetting('max_length', 255)
+      ->setDisplayOptions('form', [
+        'type' => 'string_textfield',
+        'weight' => -3,
+      ]);
+
+    $fields['reachability'] = BaseFieldDefinition::create('string')
+      ->setTranslatable(TRUE)
+      ->setLabel(t('Reachability'))
+      ->setDescription(t('Timeframe the organization is reachable.'))
+      ->setSetting('max_length', 255)
+      ->setDisplayOptions('form', [
+        'type' => 'string_textfield',
+        'weight' => -3,
+      ]);
+
+    $fields['referral'] = BaseFieldDefinition::create('string')
+      ->setTranslatable(TRUE)
+      ->setLabel(t('Referral'))
+      ->setDescription(t('Referral to youvo.'))
+      ->setSetting('max_length', 255)
+      ->setDisplayOptions('form', [
+        'type' => 'string_textfield',
+        'weight' => -5,
+      ]);
+
+    $fields['uid'] = BaseFieldDefinition::create('entity_reference')
+      ->setLabel(t('Author'))
+      ->setDescription(t('The user ID of the proposal author.'))
+      ->setSetting('target_type', 'user');
+
+    $fields['created'] = BaseFieldDefinition::create('created')
+      ->setLabel(t('Authored on'))
+      ->setDescription(t('The time that the proposal was created.'));
+
+    $fields['changed'] = BaseFieldDefinition::create('changed')
+      ->setLabel(t('Changed'))
+      ->setDescription(t('The time that the proposal was last edited.'));
+
+    return $fields;
+  }
+
+}
