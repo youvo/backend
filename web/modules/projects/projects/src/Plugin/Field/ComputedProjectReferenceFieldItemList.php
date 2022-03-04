5945dc21cb43dd9ae5b3543d967e175dc8f41e5b
Simon Baese <simon@youvo.org>
2022-03-04 20:19:07 +0900

    Add computed field for projects referencing users

---
+++ web/modules/projects/projects/src/Plugin/Field/ComputedProjectReferenceFieldItemList.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Drupal\projects\Plugin\Field;
+
+use Drupal\Core\Field\EntityReferenceFieldItemList;
+use Drupal\Core\TypedData\ComputedItemListTrait;
+
+/**
+ * Computes referencing children of parent.
+ */
+class ComputedProjectReferenceFieldItemList extends EntityReferenceFieldItemList {
+
+  use ComputedItemListTrait;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Retrieves the entity type manager.
+   *
+   * @return \Drupal\Core\Entity\EntityTypeManagerInterface
+   *   The entity type manager.
+   */
+  protected function entityTypeManager() {
+    if (!isset($this->entityTypeManager)) {
+      $this->entityTypeManager = \Drupal::entityTypeManager();
+    }
+    return $this->entityTypeManager;
+  }
+
+  /**
+   * Computes the field value.
+   *
+   * @throws \Drupal\Core\TypedData\Exception\ReadOnlyException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   */
+  protected function computeValue() {
+
+    // Fetch the user and determine respective field.
+    $account = $this->getEntity();
+    $field = $account->bundle() == 'creative' ? 'field_participants' : 'uid';
+
+    // Query projects referencing user.
+    $query = $this->entityTypeManager()
+      ->getStorage('node')->getQuery()
+      ->condition('type', 'project')
+      ->condition($field, $account->id());
+
+    // Attach the query result to the list.
+    $this->setValue(
+      array_map(fn ($id) => ['target_id' => $id], $query->execute())
+    );
+  }
+
+}
