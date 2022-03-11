51708a7d6d790dd44f4cdc116ce9196dbf116200
Simon Baese <simon@youvo.org>
2022-03-11 15:19:12 +0900

    Move create REST endpoint to organizations module

--- web/modules/youvo/user_types/organizations/src/Controller/OrganizationAccessController.php
+++ web/modules/youvo/user_types/organizations/src/Controller/OrganizationAccessController.php
@@ -38,4 +38,23 @@ class OrganizationAccessController extends ControllerBase {
     return AccessResult::neutral();
   }
 
+  /**
+   * Checks access for organization create resource.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $account
+   *   Run access checks for this account.
+   *
+   * @return \Drupal\Core\Access\AccessResultInterface
+   *   The access results.
+   */
+  public function accessCreate(AccountInterface $account) {
+
+    // Only anonymous user is allowed to create new organization.
+    if ($account->isAnonymous()) {
+      return AccessResult::allowed();
+    }
+
+    return AccessResult::neutral();
+  }
+
 }
