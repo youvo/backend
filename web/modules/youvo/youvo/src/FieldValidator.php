bf1a7a9b5e43472ce9b2c86e8e47fdaf59e51458
Simon Baese <simon@youvo.org>
2022-03-17 15:35:24 +0900

    Extend field validator

--- web/modules/youvo/youvo/src/FieldValidator.php
+++ web/modules/youvo/youvo/src/FieldValidator.php
@@ -2,22 +2,31 @@
 
 namespace Drupal\youvo;
 
-use Drupal\field\Entity\FieldConfig;
+use Drupal\Core\Field\FieldDefinitionInterface;
 
 class FieldValidator {
 
-  public static function validate(FieldConfig $field, mixed $value) {
-      $method = 'validate' . ucfirst($field->getType());
+  public static function validate(FieldDefinitionInterface $field, mixed $value) {
+    $type = strstr($field->getType(), '_', true) ?: $field->getType();
+    $method = 'validate' . ucfirst($type);
+    if (method_exists(__CLASS__, $method)) {
       return static::$method($field, $value);
+    }
+    return FALSE;
   }
 
-  public static function validateString(FieldConfig $field, mixed $value) {
+  public static function validateString(FieldDefinitionInterface $field, mixed $value) {
+    $max_length = $field->getItemDefinition()->getSetting('max_length') ?: -1;
     return is_string($value) &&
-      strlen($value) <= $field->getItemDefinition()->getSetting('max_length');
+      ($max_length == - 1 || strlen($value) <= $max_length);
   }
 
-  public static function validateBool(FieldConfig $field, mixed $value) {
+  public static function validateBool(FieldDefinitionInterface $field, mixed $value) {
     return is_bool($value);
+  }
+
+  public static function validateEmail(FieldDefinitionInterface $field, mixed $value) {
+    return \Drupal::service('email.validator')->isValid($value);
   }
 }
 
