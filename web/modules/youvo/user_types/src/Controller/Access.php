50d886cdc4750d399702e1c28fa86f2b793b85b6
Simon Baese <simon@youvo.org>
2022-03-23 17:11:20 +0900

    Add user update email endpoint

--- web/modules/youvo/user_types/src/Controller/Access.php
+++ web/modules/youvo/user_types/src/Controller/Access.php
@@ -8,7 +8,7 @@ use Drupal\Core\Session\AccountProxyInterface;
 use Drupal\user\Entity\User;
 
 /**
- * Access controller for lecture rest resources.
+ * Access controller for user update resources.
  */
 class Access extends ControllerBase {
 
@@ -22,6 +22,32 @@ class Access extends ControllerBase {
    *   The access results.
    */
   public function updatePassword(AccountProxyInterface $account) {
+
+    // Forbidden if anonymous or blocked.
+    if ($account->isAnonymous()) {
+      return AccessResult::forbidden();
+    }
+
+    // Forbidden for blocked users.
+    $account_entity = $account->getAccount();
+    if (!$account_entity instanceof User ||
+      $account_entity->isBlocked()) {
+      return AccessResult::forbidden();
+    }
+
+    return AccessResult::allowed();
+  }
+
+  /**
+   * Checks access for change email REST resources.
+   *
+   * @param \Drupal\Core\Session\AccountProxyInterface $account
+   *   Run access checks for this account.
+   *
+   * @return \Drupal\Core\Access\AccessResultInterface
+   *   The access results.
+   */
+  public function updateEmail(AccountProxyInterface $account) {
 
     // Forbidden if anonymous or blocked.
     if ($account->isAnonymous()) {
