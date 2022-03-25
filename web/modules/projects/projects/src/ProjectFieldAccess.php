7edd2b3b1b322a26c2ae17f895b31be3e93f2571
Simon Baese <simon@youvo.org>
2022-03-25 16:33:51 +0900

    First access layer for public and unrestricted fields of projects

--- web/modules/projects/projects/src/ProjectFieldAccess.php
+++ web/modules/projects/projects/src/ProjectFieldAccess.php
@@ -19,7 +19,68 @@ class ProjectFieldAccess {
    * @see projects.module projects_entity_field_access()
    */
   public static function checkFieldAccess($operation, FieldDefinitionInterface $field_definition, AccountInterface $account, FieldItemListInterface $items = NULL) {
-    return AccessResult::neutral();
+    if (self::isFieldPublic($field_definition)) {
+      return AccessResult::neutral();
+    }
+    return AccessResult::forbidden();
+  }
+
+  /**
+   * We hard-code unrestricted fields for projects here. This approach might
+   * be less flexible and lead to confusion when adding new fields. But, it is
+   * most secure when granting access to setting field values during creation
+   * of new project entities. @see ProjectRestResponder
+   */
+  public static function isFieldUnrestricted(FieldDefinitionInterface|string $field) {
+
+    // Resolve field name.
+    $field_name = $field instanceof FieldDefinitionInterface ?
+      $field->getName() : $field;
+
+    $unrestricted_fields = [
+      'body',
+      'field_allowance',
+      'field_appreciation',
+      'field_city',
+      'field_deadline',
+      'field_image',
+      'field_image_copyright',
+      'field_local',
+      'field_material',
+      'field_skills',
+      'field_workload',
+      'title',
+    ];
+
+    return in_array($field_name, $unrestricted_fields);
+  }
+
+  /**
+   * We hard-code public fields for projects here. See comment above.
+   */
+  public static function isFieldPublic(FieldDefinitionInterface|string $field) {
+
+    // Resolve field name.
+    $field_name = $field instanceof FieldDefinitionInterface ?
+      $field->getName() : $field;
+
+    // An unrestricted field is public.
+    if (self::isFieldUnrestricted($field_name)) {
+      return TRUE;
+    }
+
+    $public_fields = [
+      'created',
+      'field_contact',
+      'field_lifecycle',
+      'langcode',
+      'promote',
+      'status',
+      'sticky',
+      'uid',
+    ];
+
+    return in_array($field_name, $public_fields);
   }
 
 }
