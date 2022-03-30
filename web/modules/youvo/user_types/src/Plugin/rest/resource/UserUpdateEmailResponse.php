0434a9d2988de407555900e22dab6f873a054887
Simon Baese <simon@youvo.org>
2022-03-30 20:13:24 +0900

    Deliver more explicit messages with email change endpoint

--- web/modules/youvo/user_types/src/Plugin/rest/resource/UserUpdateEmailResponse.php
+++ web/modules/youvo/user_types/src/Plugin/rest/resource/UserUpdateEmailResponse.php
@@ -108,22 +108,28 @@ class UserUpdateEmailResponse extends ResourceBase {
 
     // Check whether email was provided.
     if (empty($email)) {
-      return new ModifiedResourceResponse('The email parameter was not provided.', 400);
-    }
-
-    // Check whether the email has changed. Nothing to do.
-    if ($email == $this->currentUser->getEmail()) {
-      return new ModifiedResourceResponse();
+      return new ModifiedResourceResponse([
+        'message' => 'The email parameter was not provided.',
+        'field' => 'email'
+      ], 400);
     }
 
     // Check whether email is valid.
     if (!$this->emailValidator->isValid($email)) {
-      return new ModifiedResourceResponse('The provided email is not valid.', 400);
+      return new ModifiedResourceResponse([
+        'message' => 'The provided email is not valid.',
+        'field' => 'email'
+      ], 400);
     }
 
     // Check if there already exists an account with given email.
-    if ($this->accountExistsForEmail($email)) {
-      return new ModifiedResourceResponse('There already exists an account for the provided email.', 409);
+    $current_email = $this->currentUser->isAuthenticated() &&
+      $this->currentUser->getEmail() == $email;
+    if ($this->accountExistsForEmail($email) && !$current_email) {
+      return new ModifiedResourceResponse([
+        'message' => 'There already exists an account for the provided email.',
+        'field' => 'email'
+      ], 409);
     }
 
     return new ModifiedResourceResponse();
@@ -151,12 +157,18 @@ class UserUpdateEmailResponse extends ResourceBase {
 
     // Check whether current_password was provided.
     if (empty($content['current_password'])) {
-      return new ResourceResponse('The value current_password was not provided.', 400);
+      return new ResourceResponse([
+        'message' => 'The value current_password was not provided.',
+        'field' => 'current_password'
+        ], 400);
     }
 
     // Check whether email was provided.
     if (empty($content['new_email'])) {
-      return new ResourceResponse('The value new_email was not provided.', 400);
+      return new ResourceResponse([
+        'message' => 'The value new_email was not provided.',
+        'field' => 'new_email'
+      ], 400);
     }
 
     // Check whether the email has changed. Nothing to do.
@@ -164,15 +176,21 @@ class UserUpdateEmailResponse extends ResourceBase {
       return new ResourceResponse();
     }
 
-    // Check whether email is valid.
+      // Check whether email is valid.
     $email = trim($content['new_email']);
     if (!$this->emailValidator->isValid($email)) {
-      return new ResourceResponse('The new email is not valid.', 400);
+      return new ResourceResponse([
+        'message' => 'The new email is not valid.',
+        'field' => 'new_email'
+      ], 400);
     }
 
     // Check if there already exists an account with given email.
     if ($this->accountExistsForEmail($email)) {
-      return new ResourceResponse('There already exists an account for the new email.', 409);
+      return new ResourceResponse([
+        'message' => 'There already exists an account for the new email.',
+        'field' => 'new_email'
+      ], 409);
     }
 
     // Load the user object from the account proxy and set existing password.
@@ -185,7 +203,10 @@ class UserUpdateEmailResponse extends ResourceBase {
     /** @var \Drupal\user\UserInterface $account_unchanged */
     $account_unchanged = $this->userStorage->loadUnchanged($account->id());
     if (!$account->checkExistingPassword($account_unchanged)) {
-      return new ResourceResponse('The provided current password is incorrect.', 409);
+      return new ResourceResponse([
+        'message' => 'The provided current password is incorrect.',
+        'field' => 'current_password'
+      ], 409);
     }
 
     /**
