75b246693a6dec6600819f5f2aeedcb57af0ce6f
Simon Baese <simon@youvo.org>
2022-04-05 18:03:35 +0900

    Add new FieldHttpException

---
+++ web/modules/youvo/src/Exception/FieldHttpException.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\youvo\Exception;
+
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class FieldHttpException extends HttpException {
+
+  private $field;
+
+  public function __construct(
+    int $statusCode,
+    ?string $message = '',
+    ?string $field = '',
+    \Throwable $previous = null,
+    array $headers = [],
+    ?int $code = 0
+  ) {
+    parent::__construct($statusCode, $message, $previous, $headers, $code);
+    $this->field = $field;
+  }
+
+  public function getField() {
+    return $this->field;
+  }
+}
