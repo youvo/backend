e9918229948b79db7ba3809c3df9749ceaa4c4a9
Simon Baese <simon@youvo.org>
2022-04-07 12:18:05 +0900

    Restrict access to question answers fields when viewing questionnaire

---
+++ web/modules/academy/questionnaire/src/QuestionFieldAccess.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Drupal\questionnaire;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Field\FieldDefinitionInterface;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\questionnaire\Entity\Question;
+use Drupal\youvo\Utility\FieldAccess;
+
+/**
+ * Provides field access methods for the question entity.
+ */
+class QuestionFieldAccess extends FieldAccess {
+
+  const ANSWER_FIELDS = [
+    'answers',
+    'explanation',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function checkFieldAccess(
+    ContentEntityInterface $entity,
+    string $operation,
+    FieldDefinitionInterface $field_definition,
+    AccountInterface $account
+  ) {
+
+    // Only question fields should be controlled by this class.
+    if (!$entity instanceof Question) {
+      return AccessResult::neutral();
+    }
+
+    // Administrators and editors pass through.
+    if ($account->hasPermission('administer site') ||
+      ($operation == 'edit ' && $account->hasPermission('manage courses'))) {
+      return AccessResult::neutral();
+    }
+
+    // Restrict accessing the question answer fields when viewing questionnaire.
+    if ($entity->getParentEntity()->bundle() == 'questionnaire' &&
+      self::isFieldOfGroup($field_definition, self::ANSWER_FIELDS)) {
+      return AccessResult::forbidden();
+    }
+
+    return AccessResult::neutral();
+  }
+
+}
