ed8b1bee9560c5414bace28990635e7bf5d13cd9
Simon Baese <simon@youvo.org>
2022-04-21 13:59:26 +0900

    Adjust project edit and create access

--- web/modules/projects/projects/src/ProjectEntityAccess.php
+++ web/modules/projects/projects/src/ProjectEntityAccess.php
@@ -3,6 +3,7 @@
 namespace Drupal\projects;
 
 use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Access\AccessResultNeutral;
 use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\node\NodeAccessControlHandler;
@@ -12,6 +13,9 @@ use Drupal\projects\Entity\Project;
  * Access handler for project entities.
  *
  * See projects module file for hook.
+ *
+ * @todo Translate role checks to permission checks and adjust caching, when
+ *   dust has settled.
  */
 class ProjectEntityAccess extends NodeAccessControlHandler {
 
@@ -25,25 +29,50 @@ class ProjectEntityAccess extends NodeAccessControlHandler {
       return parent::checkAccess($node, $operation, $account);
     }
 
-    // Check access for delete action.
-    if ($operation == 'delete') {
-      return $this->checkDeleteAccess($node, $account);
+    // Note that the access is governed by the related permissions. Therefore,
+    // one should check the permissions first that are handled in the parent
+    // method. Then, we revoke access depending on the status of the project.
+    $access_result = new AccessResultNeutral();
+
+    // Check access for edit action.
+    if ($operation == 'edit') {
+      $access_result = $this->checkEditAccess($node, $account);
     }
 
-    return parent::checkAccess($node, $operation, $account);
+    // Modify access for delete action.
+    if ($operation == 'delete') {
+      $access_result = $this->checkDeleteAccess($node, $account);
+    }
+
+    // Delegate permission check to node access handler.
+    return $access_result
+      ->orIf(parent::checkAccess($node, $operation, $account));
   }
 
   /**
-   * {@inheritdoc}
+   * Helps to check access for edit operation.
    */
-  protected function checkCreateAccess(AccountInterface $account, array $context, $entity_bundle = NULL) {
+  private function checkEditAccess(ProjectInterface $project, AccountInterface $account) {
 
-    // Only projects should be handled by this access controller.
-    if ($entity_bundle != 'project') {
-      return parent::checkCreateAccess($account, $context, $entity_bundle);
+    // Only managers of the organization can edit the project.
+    if (in_array('manager', $account->getRoles())) {
+      return AccessResult::forbiddenIf(!$project->isManager($account))
+        ->addCacheableDependency($project->getOwner())
+        ->cachePerUser();
     }
 
-    return parent::checkCreateAccess($account, $context, $entity_bundle);
+    // The organization can only edit draft, pending or open projects.
+    if ($project->isAuthor($account)) {
+      return AccessResult::forbiddenIf(
+        !$project->isPublished() ||
+        !($project->workflowManager()->isPending() ||
+        $project->workflowManager()->isDraft() ||
+        $project->workflowManager()->isOpen()))
+        ->cachePerUser()
+        ->addCacheableDependency($project);
+    }
+
+    return AccessResult::neutral();
   }
 
   /**
@@ -51,20 +80,16 @@ class ProjectEntityAccess extends NodeAccessControlHandler {
    */
   private function checkDeleteAccess(ProjectInterface $project, AccountInterface $account) {
 
-    // Supervisors and managers can delete projects.
-    if (in_array('supervisor', $account->getRoles()) ||
-      $project->isManager($account)) {
-      return AccessResult::allowed();
+    // The organization can only delete pending or draft projects.
+    if ($project->isAuthor($account) &&
+      !($project->workflowManager()->isPending() ||
+        $project->workflowManager()->isDraft())) {
+      return AccessResult::forbidden()
+        ->cachePerUser()
+        ->addCacheableDependency($project);
     }
 
-    // Managers and the organization can delete pending or draft projects.
-    if (($project->workflowManager()->isPending() ||
-        $project->workflowManager()->isDraft()) &&
-      $project->isAuthorOrManager($account)) {
-      return AccessResult::allowed();
-    }
-
-    return AccessResult::forbidden();
+    return AccessResult::neutral();
   }
 
 }
