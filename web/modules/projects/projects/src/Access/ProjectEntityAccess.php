ce60fed73da6d66a70e6b4da90c347a51fc4e96b
Simon Baese <simon@youvo.org>
2022-06-08 20:04:50 +0200

    Cover different operation identifiers in entity access check

--- web/modules/projects/projects/src/Access/ProjectEntityAccess.php
+++ web/modules/projects/projects/src/Access/ProjectEntityAccess.php
@@ -62,7 +62,7 @@ class ProjectEntityAccess extends EntityAccessControlHandler {
     }
 
     // Check access for edit action.
-    if ($operation == 'update') {
+    if ($operation == 'edit' || $operation == 'update') {
       $access_result = $this->checkEditAccess($entity, $account);
     }
 
