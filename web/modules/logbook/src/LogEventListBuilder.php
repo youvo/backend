47fcf6bfcaaa64ec6f4b796cf1cfcb86e75a9404
Simon Baese <simon@youvo.org>
2022-06-09 19:17:47 +0200

    Use dependency injection in log event list builder

--- web/modules/logbook/src/LogEventListBuilder.php
+++ web/modules/logbook/src/LogEventListBuilder.php
@@ -3,11 +3,55 @@
 namespace Drupal\logbook;
 
 use Drupal\Core\Entity\EntityListBuilder;
+use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Entity\EntityViewBuilderInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a list controller for the log event entity type.
  */
-class LogEventListBuilder extends EntityListBuilder {
+final class LogEventListBuilder extends EntityListBuilder {
+
+  /**
+   * The log event view builder.
+   *
+   * @var \Drupal\Core\Entity\EntityViewBuilderInterface
+   */
+  protected EntityViewBuilderInterface $logEventViewBuilder;
+
+  /**
+   * Constructs a new LectureListBuilder object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
+   *   The entity type definition.
+   * @param \Drupal\Core\Entity\EntityStorageInterface $storage
+   *   The entity storage class.
+   * @param \Drupal\Core\Entity\EntityViewBuilderInterface $log_event_view_builder
+   *   The log event view builder.
+   */
+  public function __construct(
+    EntityTypeInterface $entity_type,
+    EntityStorageInterface $storage,
+    EntityViewBuilderInterface $log_event_view_builder
+  ) {
+    parent::__construct($entity_type, $storage);
+    $this->logEventViewBuilder = $log_event_view_builder;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function createInstance(
+    ContainerInterface $container,
+    EntityTypeInterface $entity_type
+  ) {
+    return new self(
+      $entity_type,
+      $container->get('entity_type.manager')->getStorage($entity_type->id()),
+      $container->get('entity_type.manager')->getViewBuilder('log_event'),
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -15,9 +59,8 @@ class LogEventListBuilder extends EntityListBuilder {
   public function render() {
 
     $build['table']['#prefix'] = '<div class="system-status-general-info__items clearfix">';
-    $view_builder = \Drupal::entityTypeManager()->getViewBuilder('log_event');
     foreach ($this->load() as $entity) {
-      if ($row = $view_builder->view($entity)) {
+      if ($row = $this->logEventViewBuilder->view($entity)) {
         $build['table'][] = $row;
       }
     }
