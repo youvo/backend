a7cfbdecf5c17ca16070edbc0d8b7bf2d7841236
Simon Baese <simon@youvo.org>
2022-06-09 18:34:10 +0200

    Provide simple list builder for log events

--- web/modules/logbook/src/LogEventListBuilder.php
+++ web/modules/logbook/src/LogEventListBuilder.php
@@ -2,13 +2,7 @@
 
 namespace Drupal\logbook;
 
-use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityListBuilder;
-use Drupal\Core\Datetime\DateFormatterInterface;
-use Drupal\Core\Entity\EntityStorageInterface;
-use Drupal\Core\Entity\EntityTypeInterface;
-use Drupal\Core\Routing\RedirectDestinationInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a list controller for the log event entity type.
@@ -16,101 +10,19 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class LogEventListBuilder extends EntityListBuilder {
 
   /**
-   * The date formatter service.
-   *
-   * @var \Drupal\Core\Datetime\DateFormatterInterface
-   */
-  protected $dateFormatter;
-
-  /**
-   * The redirect destination service.
-   *
-   * @var \Drupal\Core\Routing\RedirectDestinationInterface
-   */
-  protected $redirectDestination;
-
-  /**
-   * Constructs a new LogEventListBuilder object.
-   *
-   * @param \Drupal\Core\Entity\EntityTypeInterface $entity_type
-   *   The entity type definition.
-   * @param \Drupal\Core\Entity\EntityStorageInterface $storage
-   *   The entity storage class.
-   * @param \Drupal\Core\Datetime\DateFormatterInterface $date_formatter
-   *   The date formatter service.
-   * @param \Drupal\Core\Routing\RedirectDestinationInterface $redirect_destination
-   *   The redirect destination service.
-   */
-  public function __construct(EntityTypeInterface $entity_type, EntityStorageInterface $storage, DateFormatterInterface $date_formatter, RedirectDestinationInterface $redirect_destination) {
-    parent::__construct($entity_type, $storage);
-    $this->dateFormatter = $date_formatter;
-    $this->redirectDestination = $redirect_destination;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function createInstance(ContainerInterface $container, EntityTypeInterface $entity_type) {
-    return new static(
-      $entity_type,
-      $container->get('entity_type.manager')->getStorage($entity_type->id()),
-      $container->get('date.formatter'),
-      $container->get('redirect.destination')
-    );
-  }
-
-  /**
    * {@inheritdoc}
    */
   public function render() {
-    $build['table'] = parent::render();
 
-    $total = $this->getStorage()
-      ->getQuery()
-      ->count()
-      ->accessCheck(FALSE)
-      ->execute();
-
-    $build['summary']['#markup'] = $this->t('Total log events: @total', ['@total' => $total]);
-    return $build;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function buildHeader() {
-    $header['id'] = $this->t('ID');
-    $header['uid'] = $this->t('Author');
-    $header['created'] = $this->t('Created');
-    return $header + parent::buildHeader();
-  }
-
-  /**
-   * {@inheritdoc}
-   *
-   * @throws \Drupal\Core\Entity\EntityMalformedException
-   */
-  public function buildRow(EntityInterface $entity) {
-    /** @var \Drupal\logbook\LogEventInterface $entity */
-    $row['id'] = $entity->toLink();
-    $row['uid']['data'] = [
-      '#theme' => 'username',
-      '#account' => $entity->getOwner(),
-    ];
-    $row['created'] = $this->dateFormatter->format($entity->getCreatedTime());
-    return $row + parent::buildRow($entity);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function getDefaultOperations(EntityInterface $entity) {
-    $operations = parent::getDefaultOperations($entity);
-    $destination = $this->redirectDestination->getAsArray();
-    foreach ($operations as $key => $operation) {
-      $operations[$key]['query'] = $destination;
+    $build['table']['#prefix'] = '<div class="system-status-general-info__items clearfix">';
+    $view_builder = \Drupal::entityTypeManager()->getViewBuilder('log_event');
+    foreach ($this->load() as $entity) {
+      if ($row = $view_builder->view($entity)) {
+        $build['table'][] = $row;
+      }
     }
-    return $operations;
+    $build['table']['#suffix'] = '</div>';
+    return $build;
   }
 
 }
