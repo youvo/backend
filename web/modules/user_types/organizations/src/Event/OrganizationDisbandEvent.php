91fd22405f357544b349e74ea193790bba50ac82
Simon Baese <simon@youvo.org>
2022-06-21 12:11:18 +0200

    Add organization manage and disband logs

---
+++ web/modules/user_types/organizations/src/Event/OrganizationDisbandEvent.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Drupal\organizations\Event;
+
+/**
+ * Defines an organization disband event.
+ *
+ * When a manager decided not to manage an organization anymore.
+ *
+ * @see OrganizationManageResource::delete
+ */
+class OrganizationDisbandEvent extends OrganizationManageEvent {
+
+}
---
+++ web/modules/user_types/organizations/src/Event/OrganizationManageEvent.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Drupal\organizations\Event;
+
+use Drupal\Component\EventDispatcher\Event;
+use Drupal\creatives\Entity\Creative;
+use Drupal\organizations\Entity\Organization;
+
+/**
+ * Defines an organization manage event.
+ *
+ * @see OrganizationManageResource::post
+ */
+class OrganizationManageEvent extends Event {
+
+  /**
+   * Constructs a OrganizationManageEvent object.
+   *
+   * @param \Drupal\organizations\Entity\Organization $organization
+   *   The organization.
+   * @param \Drupal\creatives\Entity\Creative $manager
+   *   The manager.
+   */
+  public function __construct(
+    protected Organization $organization,
+    protected Creative $manager
+  ) {}
+
+  /**
+   * Gets the organization.
+   */
+  public function getOrganization() {
+    return $this->organization;
+  }
+
+  /**
+   * Gets the manager.
+   */
+  public function getManager() {
+    return $this->manager;
+  }
+
+}
