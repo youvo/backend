c282f311aa8d0b9f82ab7229f6cc522e7e04f1c2
Simon Baese <simon@youvo.org>
2022-06-21 15:21:23 +0200

    Add some stats and template for overview page

--- web/modules/monitoring/stats/src/Controller/StatsController.php
+++ web/modules/monitoring/stats/src/Controller/StatsController.php
@@ -3,6 +3,8 @@
 namespace Drupal\stats\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\stats\StatsCalculator;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Controller for stats pages.
@@ -10,11 +12,31 @@ use Drupal\Core\Controller\ControllerBase;
 class StatsController extends ControllerBase {
 
   /**
+   * Construct stats overview controller with services.
+   *
+   * @param \Drupal\stats\StatsCalculator $statsCalculator
+   *   The stat calculator service.
+   */
+  public function __construct(protected StatsCalculator $statsCalculator) {}
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static($container->get('stats.calculator'));
+  }
+
+  /**
    * Controls overview.
    */
   public function overview() {
 
-    $page = [];
+    $page['creatives'] = $this->statsCalculator->countCreatives();
+    $page['organizations'] = $this->statsCalculator->countOrganizations();
+    $page['open_projects'] = $this->statsCalculator->countOpenProjects();
+    $page['ongoing_projects'] = $this->statsCalculator->countOngoingProjects();
+    $page['completed_projects'] = $this->statsCalculator->countCompletedProjects();
+    $page['mediated_projects'] = $this->statsCalculator->countMediatedProjects();
 
     return [
       '#theme' => 'stats-overview',
