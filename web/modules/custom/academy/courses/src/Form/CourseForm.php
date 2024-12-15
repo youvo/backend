9e138771b7727a295b73dd80559198ca0ac4371a
Simon Baese <simon@youvo.org>
2024-12-15 16:27:09 +0100

    Improve typing in courses module

--- web/modules/custom/academy/courses/src/Form/CourseForm.php
+++ web/modules/custom/academy/courses/src/Form/CourseForm.php
@@ -5,6 +5,7 @@ namespace Drupal\courses\Form;
 use Drupal\Core\Entity\ContentEntityForm;
 use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\courses\Entity\Course;
 use Drupal\youvo\TranslationFormButtonsTrait;
 
 /**
@@ -19,7 +20,7 @@ class CourseForm extends ContentEntityForm {
    *
    * @throws \Drupal\Core\Entity\EntityMalformedException
    */
-  public function buildForm(array $form, FormStateInterface $form_state) {
+  public function buildForm(array $form, FormStateInterface $form_state): array {
     $form = parent::buildForm($form, $form_state);
 
     /** @var \Drupal\courses\Entity\Course $course */
@@ -35,7 +36,7 @@ class CourseForm extends ContentEntityForm {
       '#maxlength' => EntityTypeInterface::BUNDLE_MAX_LENGTH,
       '#disabled' => !$course->isNew(),
       '#machine_name' => [
-        'exists' => ['Drupal\courses\Entity\Course', 'load'],
+        'exists' => [Course::class, 'load'],
         'source' => ['title', 'widget', 0, 'value'],
       ],
       '#description' => $this->t('A unique machine-readable name for this content type. It must only contain lowercase letters, numbers, and underscores.'),
@@ -44,7 +45,7 @@ class CourseForm extends ContentEntityForm {
 
     if (!$course->isNew() &&
       $course->getEntityType()->hasLinkTemplate('drupal:content-translation-overview')) {
-      static::addTranslationButtons($form, $course);
+      $this->addTranslationButtons($form, $course);
     }
 
     return $form;
@@ -55,13 +56,13 @@ class CourseForm extends ContentEntityForm {
    *
    * @throws \Drupal\Core\Entity\EntityStorageException
    */
-  public function save(array $form, FormStateInterface $form_state) {
+  public function save(array $form, FormStateInterface $form_state): int {
 
     $entity = $this->getEntity();
     $result = $entity->save();
     $message_arguments = ['%label' => $this->entity->label()];
 
-    if ($result == SAVED_NEW) {
+    if ($result === SAVED_NEW) {
       $this->messenger()->addStatus($this->t('New course %label has been created.', $message_arguments));
       $this->logger('academy')->notice('Created new course %label.', $message_arguments);
     }
