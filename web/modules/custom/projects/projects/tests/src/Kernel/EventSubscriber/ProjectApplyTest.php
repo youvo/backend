4029a1310f4a157b8bb345ec23188ca04e5d8f81
Simon Baese <simon@youvo.org>
2024-12-19 23:11:29 +0100

    Add test for project apply action

---
+++ web/modules/custom/projects/projects/tests/src/Kernel/EventSubscriber/ProjectApplyTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Drupal\Tests\projects\Kernel\EventSubscriber;
+
+use Drupal\projects\Event\ProjectApplyEvent;
+use Drupal\projects\ProjectState;
+
+/**
+ * Tests for the project apply event subscriber.
+ *
+ * @coversDefaultClass \Drupal\projects\EventSubscriber\Action\ProjectApplySubscriber
+ * @group projects
+ */
+class ProjectApplyTest extends ProjectEventSubscriberTestBase {
+
+  /**
+   * Tests the project apply event listener.
+   *
+   * @covers ::onProjectApply
+   * @covers ::getSubscribedEvents
+   */
+  public function testProjectApply(): void {
+
+    $project = $this->createProject(ProjectState::OPEN);
+    $this->assertTrue($project->lifecycle()->isOpen());
+
+    $creative = $this->createCreative();
+    $event = new ProjectApplyEvent($project, $creative);
+    $event->setPhoneNumber('+1234567890');
+    $this->eventDispatcher->dispatch($event);
+
+    $this->assertTrue($project->lifecycle()->isOpen());
+    $this->assertTrue($project->hasApplicant());
+    $this->assertTrue($project->isApplicant($creative));
+
+    $applicants = $project->getApplicants();
+    $this->assertCount(1, $applicants);
+    $applicant = reset($applicants);
+    $this->assertEquals('+1234567890', $applicant->getPhoneNumber());
+  }
+
+}
